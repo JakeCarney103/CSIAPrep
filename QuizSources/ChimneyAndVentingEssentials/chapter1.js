1eb0ca809916686d4f30bdbf62cf7fb1a95906f8
Jake Carney <jakecarney103@gmail.com>
2026-01-03 16:26:30 -0500

    Add Chimney and Venting Essentials Chapter 1

---
+++ QuizSources/ChimneyAndVentingEssentials/chapter1.js
@@ -0,0 +1,35 @@
+const essentialsChapter1QuizData = [
+  {
+    question: "What decade did the chimney sweep trade experience a rebirth? (Page 1)",
+    options: ["1970s", "1980s", "1990s", "2000s"],
+    answer: "1970s"
+  },
+  {
+    question: "Why did the chimney sweep trade experience a rebirth? (Page 1)",
+    options: ["Popularity increase due to public media", "Because of Dick Van Dyke", "Energy Crisis caused people to burn more wood", "None of the above"],
+    answer: "Energy Crisis caused people to burn more wood"
+  },
+  {
+    question: "Chimney Sweeps are now considered _________ due to an increase in knowledge and skill. (Page 1)",
+    options: ["Chimney Instructors", "Firemen", "Chimney Technicians", "AHJs"],
+    answer: "Chimney Technicians"
+  },
+  {
+    question: "Every jurisdiction could have carying regulations. Some mandated by the state; others are local. (Page 2)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "Many code sections daling with chimneys, fireplaces, solid fuel appliances, connectors, and venting systems are based on the _________. (Page 2)",
+    options: ["UL 103", "NFPA 54", "NFPA 211", "ASTM E-1509"],
+    answer: "NFPA 211"
+  },
+  {
+    question: "The Chimney And Venting Essentials manual is only a reference manual and should not be considered an industry standard, code or the applicable law. (Page 3)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+];
+
+if (!window.quizRegistry) window.quizRegistry = {};
+window.quizRegistry['Chapter 1: Role of the Modern Chimney Technician'] = essentialsChapter1QuizData;
