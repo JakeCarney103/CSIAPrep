5a35e473da32ab50bddb222c038ff7e2235e56a3
Jake Carney <jake.carney@taxcredible.com>
2026-01-03 21:38:48 -0500

    Add Chimney and Venting Essentials Chapter 2

---
+++ QuizSources/ChimneyAndVentingEssentials/chapter2.js
@@ -0,0 +1,340 @@
+const essentialsChapter2QuizData = [
+  {
+    question: "_________ is the ultimate authority for approving installations. They may be an individual or an organization like the local building department or building inspector or the fire marshal. (Page 5)",
+    options: ["The Installer", "The Homeowner", "The Authority Having Jurisdiction", "The Manufacturer"],
+    answer: "The Authority Having Jurisdiction"
+  },
+  {
+    question: "How often are Codes, Standards and Regulations usually updated? (Page 5)",
+    options: ["Every Year", "Every 3 Years", "Every 5 Years", "Every 10 Years"],
+    answer: "Every 3 Years"
+  },
+  {
+    question: "Whenever a new code is written, your local municipality or AHJ automatically adopts it. (Page 5)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "A _________ is a set of technical definitions, product specifications, and guidelines for the proper construction of the product. (Page 5)",
+    options: ["Code", "Regulation", "Standard", "Installation Manual"],
+    answer: "Standard"
+  },
+  {
+    question: "Simplified, a Standard tells you what? (Page 5)",
+    options: ["What laws to follow", "How to do something", "What needs to be done, but not how to do it", "None of the above"],
+    answer: "How to do something"
+  },
+  {
+    question: "A _________ is a set of rules that experts recommend people to follow; it is a model. (Page 6)",
+    options: ["Code", "Regulation", "Standard", "Installation Manual"],
+    answer: "Code"
+  },
+  {
+    question: "Simplified, a Code tells you what? (Page 6)",
+    options: ["What laws to follow", "How to do something", "What needs to be done, but not how to do it", "None of the above"],
+    answer: "What needs to be done, but not how to do it"
+  },
+  {
+    question: "A _________ is an official rule, law, or order stating what may or may not be done or how something must be done. (Page 6)",
+    options: ["Code", "Regulation", "Standard", "Installation Manual"],
+    answer: "Regulation"
+  },
+  {
+    question: "Which of the following can be adopted into law (become a Regulation)? (Page 6)",
+    options: ["Code Only", "Standard Only", "Code and Standard", "Neither"],
+    answer: "Code and Standard"
+  },
+  {
+    question: "The AHJ's installation requirements usually stem from... (Page 6)",
+    options: ["Model Codes", "Safety Standards", "Appliance Manufacturer's Instructions", "All the above"],
+    answer: "All the above"
+  },
+  {
+    question: "_________ is the most revaland document to those in the solid fuel industry. (Page 7)",
+    options: ["NFPA 31", "NFPA 54", "NFPA 211", "IRC"],
+    answer: "NFPA 211"
+  },
+  {
+    question: "NFPA, ICC, and ASTM code and standards are only recommendations and are not enforcable until adopted by a(n) _________ with enforcement power. (Page 7)",
+    options: ["Business", "Installer", "AHJ", "Laboratory"],
+    answer: "AHJ"
+  },
+  {
+    question: "The AHJ can NEVER overrule a provision of the installation instructions to satisfy a local code or insurance requirement. (Page 7)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "There are people or organizations other than the AHJ that can 'approve' an installation. (Page 7)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "There is no such thing as an 'Underwriters Laboratory approved' product. UL and other recognized testing agencies merely 'list' or 'classify' the product. (Page 7)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "By its own description, UL (Underwriters Laboratories) 'certifies, validates, tests, verifies, inspects, audits, advises and educates.' (Page 7)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "A product that has been safely tested by UL is _________. (Page 8)",
+    options: ["Tested to the UL Standard", "UL Tested", "UL Listed", "Listed"],
+    answer: "UL Listed"
+  },
+  {
+    question: "A product that has been tested by another lab other than the UL is _________. (Page 8)",
+    options: ["Tested to the UL Standard", "UL Tested", "UL Listed", "Listed"],
+    answer: "Tested to the UL Standard"
+  },
+  {
+    question: "Listing for the 'Standard for Factory-Built Chimneys for Residential Type and Building Heating Appliances'. (Page 8)",
+    options: ["UL 103", "UL 441", "UL 641", "UL 1777"],
+    answer: "UL 103"
+  },
+  {
+    question: "If a Factory-Built Chimney is tested to _________, they are given a UL 103 listing. (Page 8)",
+    options: ["1000°F", "1500°F", "1700°F", "2100°F"],
+    answer: "1700°F"
+  },
+  {
+    question: "Listing for the 'Standard for Factory-Built Fireplaces'. (Page 8)",
+    options: ["UL 103", "UL 127", "UL 462", "UL 1777"],
+    answer: "UL 127"
+  },
+  {
+    question: "Listing for the 'Standard for Solid-Fuel and Combination-Fuel Central and Supplementary Furnaces'. (Page 8)",
+    options: ["UL 391", "UL 127", "UL 462", "UL 1777"],
+    answer: "UL 391"
+  },
+  {
+    question: "Listing for the 'Standard for Gas Vents'. (Page 8)",
+    options: ["UL 391", "UL 127", "UL 462", "UL 441"],
+    answer: "UL 441"
+  },
+  {
+    question: "Listing for the 'Standard for Type-L Low Temperature Venting Systems'. (Page 8)",
+    options: ["UL 391", "UL 641", "UL 462", "UL 1777"],
+    answer: "UL 641"
+  },
+  {
+    question: "Listing for the 'Standard for Fireplace Stoves'. (Page 9)",
+    options: ["UL 391", "UL 127", "UL 462", "UL 737"],
+    answer: "UL 737"
+  },
+  {
+    question: "Listing for the 'Standard for Oil-Burning Stoves'. (Page 9)",
+    options: ["UL 896", "UL 127", "UL 462", "UL 737"],
+    answer: "UL 896"
+  },
+  {
+    question: "Listing for the 'Standard for Fireplace Accessories'. (Page 9)",
+    options: ["UL 896", "UL 127", "UL 462", "UL 907"],
+    answer: "UL 907"
+  },
+  {
+    question: "Listing for the 'Standard for Solid-Fuel Type Room Heaters'. (Page 9)",
+    options: ["UL 1482", "UL 127", "UL 462", "UL 907"],
+    answer: "UL 1482"
+  },
+  {
+    question: "Listing for the 'Standard for Wall Protectors, Floor Protectors, and Hearth Extensions'. (Page 9)",
+    options: ["UL 1482", "UL 127", "UL 1681", "UL 907"],
+    answer: "UL 1681"
+  },
+  {
+    question: "Listing for the 'Standard for Chimney Liners'. (Page 9)",
+    options: ["UL 1777", "UL 127", "UL 1681", "UL 907"],
+    answer: "UL 1777"
+  },
+  {
+    question: "Listing for the 'Standard for Processed Solid-Fuel Firelogs and Firestarters'. (Page 9)",
+    options: ["UL 1777", "UL 127", "UL 1681", "UL 2115"],
+    answer: "UL 2115"
+  },
+  {
+    question: "Listing for the 'Standard for Solid Fuel-fired Water Heaters and Boilers'. (Page 10)",
+    options: ["UL 1777", "UL 2523", "UL 1681", "UL 2115"],
+    answer: "UL 2523"
+  },
+  {
+    question: "Standard for Fireplaces, Chimneys, Vents and Solid Fuel Burning Appliances. (Page 10)",
+    options: ["NFPA 211", "NFPA 31", "NFPA 10", "NFPA 54"],
+    answer: "NFPA 211"
+  },
+  {
+    question: "Standard for Installation of Oil Fired Equipment. (Page 10)",
+    options: ["NFPA 211", "NFPA 31", "NFPA 10", "NFPA 54"],
+    answer: "NFPA 211"
+  },
+  {
+    question: "National Fuel Gas Code. (Page 10)",
+    options: ["NFPA 211", "NFPA 31", "NFPA 10", "NFPA 54"],
+    answer: "NFPA 211"
+  },
+  {
+    question: "Standard for 'Vented Gas Fireplaces'. (Page 11)",
+    options: ["ANSI Z21.50", "ANSI Z21.60", "ANSI Z21.88", "ANSI Z21.11.2"],
+    answer: "ANSI Z21.50"
+  },
+  {
+    question: "Standard for 'Decorative Gas Appliances for Installation in Solid-fuel Fireplaces'. (Page 11)",
+    options: ["ANSI Z21.50", "ANSI Z21.60", "ANSI Z21.88", "ANSI Z21.11.2"],
+    answer: "ANSI Z21.60"
+  },
+  {
+    question: "Standard for 'Vented Gas Fireplace Heaters'. (Page 11)",
+    options: ["ANSI Z21.50", "ANSI Z21.60", "ANSI Z21.88", "ANSI Z21.11.2"],
+    answer: "ANSI Z21.88"
+  },
+  {
+    question: "Standard for 'Gas-Fired Room Heaters, Volume II, Unvented Room Heaters'. (Page 11)",
+    options: ["ANSI Z21.50", "ANSI Z21.60", "ANSI Z21.88", "ANSI Z21.11.2"],
+    answer: "ANSI Z21.11.2"
+  },
+  {
+    question: "Standard Specification for Room Heaters, Pellet Fuel Burning Type. (Page 12)",
+    options: ["ASTM E-1509", "ASTM C315", "ASTM C1261"],
+    answer: "ASTM E-1509"
+  },
+  {
+    question: "Standard Specification for Clay Flue Liners and Chimney Pots. (Page 12)",
+    options: ["ASTM E-1509", "ASTM C315", "ASTM C1261"],
+    answer: "ASTM C315"
+  },
+  {
+    question: "Standard Specification for Firebox Brick for Residential Fireplaces. (Page 12)",
+    options: ["ASTM E-1509", "ASTM C315", "ASTM C1261"],
+    answer: "ASTM C1261"
+  },
+  {
+    question: "This body was formed by the 3 major building code organizations, BOCA, SBCCI and ICBO, in the 1990s when they decided to work together to develop a unified model building code for the entire country. These codes consist of 14 individual codes that cover a wide range of building topics. (Page 12)",
+    options: ["International Residential Code (IRC)", "International Building Code (IBC)", "International Code Council (ICC)", "International Existing Building Code (IEBC)"],
+    answer: "International Code Council (ICC)"
+  },
+  {
+    question: "This code establishes minimum regulations for one and two family dewllings and townhouses using prescriptive provisions. (Page 12)",
+    options: ["International Residential Code (IRC)", "International Building Code (IBC)", "International Code Council (ICC)", "International Existing Building Code (IEBC)"],
+    answer: "International Residential Code (IRC)"
+  },
+  {
+    question: "This code addresses structural strength, means of egress, sanitation, adequate lighting and ventilation, accessibility, energy conservation, and life safety regarding new and existing building, facilities, and systems. This applies to all occupancies, buildings and structures. (Page 13)",
+    options: ["International Residential Code (IRC)", "International Building Code (IBC)", "International Code Council (ICC)", "International Existing Building Code (IEBC)"],
+    answer: "International Building Code (IBC)"
+  },
+  {
+    question: "This code regulates the design and installation of mechanical systems, appliances, appliance venting, duct and ventilation systems, combustion air provisions, hydronic systems, and solar systems. (Page 13)",
+    options: ["International Fuel Gas Code (IFGC)", "International Mechanical Code (IMC)", "International Code Council (ICC)", "International Existing Building Code (IEBC)"],
+    answer: "International Mechanical Code (IMC)"
+  },
+  {
+    question: "This code applies to the installation of fuel-gas piping systems, fuel-gas utilization equipment, fuel gas venting systems and gaseous hydrogen systems and related accessories. (Page 13)",
+    options: ["International Fuel Gas Code (IFGC)", "International Mechanical Code (IMC)", "International Code Council (ICC)", "International Existing Building Code (IEBC)"],
+    answer: "International Fuel Gas Code (IFGC)"
+  },
+  {
+    question: "The International Fuel Gas Code (IFGC) has an exception for the following: Detached one and two family dwellings and multiple single family dewllings (Townhouses) not more than 3 stories high with separate means of egress, and their accessory structures shall comply with the IRC. (Page 13)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "This code contains requirements intended to encourage the use and reuse of existing buildings by covering repair, alteration, addition, and change of occupancy for existing buildings and historic buldings. (Page 14)",
+    options: ["International Fuel Gas Code (IFGC)", "International Mechanical Code (IMC)", "International Code Council (ICC)", "International Existing Building Code (IEBC)"],
+    answer: "International Existing Building Code (IEBC)"
+  },
+  {
+    question: "These codes have been created to increase efficiency and decrease wasted energy. (Page 14)",
+    options: ["'Green' Codes", "'Efficient' Codes", "'Blue' Codes", "'Red' Codes"],
+    answer: "'Green' Codes"
+  },
+  {
+    question: "This code establishes minimum regulations for energy efficient buildings. (Page 14)",
+    options: ["International Energy Conservation Code (IECC)", "Leadership in Energy and Environmental Design (LEED)", "National Green Building Standard (ICC 700)", "International Green Construction Code (IgCC)"],
+    answer: "International Energy Conservation Code (IECC)"
+  },
+  {
+    question: "This is a voluntary certification program developed by the U.S. Green Building Council and runs on a raiting system for builders. The basic requirement in this for fireplaces and wood stoves is that they must have doors. (Page 14)",
+    options: ["International Energy Conservation Code (IECC)", "Leadership in Energy and Environmental Design (LEED)", "National Green Building Standard (ICC 700)", "International Green Construction Code (IgCC)"],
+    answer: "Leadership in Energy and Environmental Design (LEED)"
+  },
+  {
+    question: "This was developed by the National Association of Home Builders and adoped by the ICC. It provides a rating system with progressive performance levels determined by meeting minimum point levels in building categories (such as energy efficiency, water efficiency, indoor air quality, etc.) and minimum overall point totals. This awards points for not installing hearth products or various points when they are safety listed and EPA certified. (Page 15)",
+    options: ["International Energy Conservation Code (IECC)", "Leadership in Energy and Environmental Design (LEED)", "National Green Building Standard (ICC 700)", "International Green Construction Code (IgCC)"],
+    answer: "National Green Building Standard (ICC 700)"
+  },
+  {
+    question: "This is a mandatory baseline standard for the construction of high-performance green buildings. The current version indicates that the code will not apply to detached one and two family dwellings. (Page 15)",
+    options: ["International Energy Conservation Code (IECC)", "Leadership in Energy and Environmental Design (LEED)", "National Green Building Standard (ICC 700)", "International Green Construction Code (IgCC)"],
+    answer: "International Green Construction Code (IgCC)"
+  },
+  {
+    question: "Why did the Environmental Protection Agency (EPA) implement the first New Source Performance Standard (NSPS)? (Page 15)",
+    options: ["Increase Energy Efficiency", "Limit air pollution from wood stoves", "Loosen government regulation on heating appliances", "To allow each state to have a different set of requirements"],
+    answer: "Limit air pollution from wood stoves"
+  },
+  {
+    question: "In 2015 the US EPA released a revied version of NSPS. Step 1 of this revision brings the limit of emissions down to _________ for all wood burning stoves. (Page 16)",
+    options: ["4 grams per hour", "4.5 grams per hour", "5 grams per hour", "5.5 grams per hour"],
+    answer: "4.5 grams per hour"
+  },
+  {
+    question: "In 2015 the US EPA released a revied version of NSPS. Step 2 of this revision brings the limit of emissions down to _________ for all wood burning stoves. (Page 16)",
+    options: ["2 or 3 grams per hour", "2 or 2.5 grams per hour", "4.5 grams per hour", "4 or 5 grams per hour"],
+    answer: "2 or 2.5 grams per hour"
+  },
+  {
+    question: "What EPA program was designed to help educate the fireplace/appliance user on tips to burn wood more efficiently. (Page 16)",
+    options: ["EPA Wood Burning Education", "EPA Burn Wise", "EPA Efficient Burning", "EPA Emission Lowering"],
+    answer: "EPA Burn Wise"
+  },
+  {
+    question: "The US Department of Housing and Urban Development (HUD) provides specifications for the _________. (Page 17)",
+    options: ["3 or More Story Safety Standards", "Apartment Construction Standards", "Single and Multi Family Dwellings Standards ", "Manufactured Home and Mobile Homes Construction and Safety Standards"],
+    answer: "Manufactured Home and Mobile Homes Construction and Safety Standards"
+  },
+  {
+    question: "According to the HUD's requirements around installing solid-fuel appliances in Manufactured and Mobile Homes, the listed factory-built chimney must be attached directly to _________. (Page 17)",
+    options: ["The side of the home", "The roof", "The fireplace or stove", "The foundation"],
+    answer: "The fireplace or stove"
+  },
+  {
+    question: "According to the HUD's requirements around installing solid-fuel appliances in Manufactured and Mobile Homes, _________ must be supplied. (Page 17)",
+    options: ["Instructions", "Combustion Air", "Manufactured Parts", "Permits"],
+    answer: "Combustion Air"
+  },
+  {
+    question: "According to the HUD's requirements around installing solid-fuel appliances in Manufactured and Mobile Homes, the appliance must be secured to the structure to avoid displacement. (Page 17)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "According to the HUD's requirements around installing solid-fuel appliances in Manufactured and Mobile Homes, the appliance must have _________. (Page 17)",
+    options: ["Accessories", "An Open Firebox", "Doors", "Legs"],
+    answer: "Doors"
+  },
+  {
+    question: "According to the HUD's requirements around installing solid-fuel appliances in Manufactured and Mobile Homes, the cannot be installed where? (Page 17)",
+    options: ["Kitchen", "Bathroom", "Any Sleeping Room", "Living Room"],
+    answer: "Any Sleeping Room"
+  },
+  {
+    question: "Some jursidictions require a special license to install gas piping, attach a gas-fired appliance to the piping, install a wood stove, install a pellet stove, reline a chimney, or even do masonry repairs. Therefore, it is a good idea to contact State or Local Departments to check regulations. (Page 17)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "The CSIA certification alone constitues a license. (Page 18)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "The CSIA certification is yours, not the company's. (Page 18)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+];
+
+if (!window.quizRegistry) window.quizRegistry = {};
+window.quizRegistry['Chapter 2: Codes, Standards & Regulations'] = essentialsChapter2QuizData;
