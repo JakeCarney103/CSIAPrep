837ea4b7a0e759e2f4a0ed298fdd521d82ba0b46
Jake Carney <jakecarney103@gmail.com>
2026-01-05 06:28:03 -0500

    Add partial chapter 3 for essentials

---
+++ QuizSources/ChimneyAndVentingEssentials/chapter3.js
@@ -0,0 +1,171 @@
+const essentialsChapter3QuizData = [
+  {
+    question: "In certain situations, jobs are so critical that they can be done in haste and with a disregard to you personal safety. (Page 19)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "How should the ladder be secured during transportation? (Page 20)",
+    options: ["Secured at only one rack support point", "Secured at each rack support point with maximum overhang", "It doesn't matter. Just throw it on the vehicle.", "Secured at each rack support point with minimum overhang"],
+    answer: "Secured at each rack support point with minimum overhang"
+  },
+  {
+    question: "Before moving the vehicle, you should _________. (Page 20)",
+    options: ["Call the office", "Walk around the vehicle to be sure everything is in order and secure", "Yell BYE at the homeowner", "Eat your lunch"],
+    answer: "Walk around the vehicle to be sure everything is in order and secure"
+  },
+  {
+    question: "Before you set up your ladder, you should _________. (Page 21)",
+    options: ["Carry the ladder to the location vertically", "Walk around the house to be sure that you have chosen the best location", "Always use the porch since it is ususally clear", "Ignore power lines"],
+    answer: "Walk around the house to be sure that you have chosen the best location"
+  },
+  {
+    question: "You should always carry the ladder _________. (Page 21)",
+    options: ["By dragging it on the ground", "Vertically", "Horizontally", "Above your head"],
+    answer: "Horizontally"
+  },
+  {
+    question: "As long as you are not touching the powerline with your ladder, you are fine. (Page 21)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "What distance can electricity jump? (Page 21)",
+    options: ["Several Feet", "Under 1 foot", "It can't, it must be contacted", "Over 30 yards"],
+    answer: "Several Feet"
+  },
+  {
+    question: "It is alright to have different surfaces under your ladder's legs. For example: It is ok for one leg to be on the grass, while the other is on the sidewalk. (Page 21)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "At a minimum how many rungs above the roof should your ladder be? (Page 21)",
+    options: ["1", "2", "3", "4"],
+    answer: "3"
+  },
+  {
+    question: "At a minimum how many inches above the roof should your ladder be? (Page 21)",
+    options: ["12", "24", "36", "48"],
+    answer: "36"
+  },
+  {
+    question: "When placing a ladder, it should be placed at a _________ angle. (Page 21)",
+    options: ["3:1", "4:1", "5:1", "2:1"],
+    answer: "4:1"
+  },
+  {
+    question: "If a ladder is set up to be 36 ft tall, how far away from the house should the base of the ladder be? (Page 21)",
+    options: ["4 feet", "6 feet", "9 feet", "12 feet"],
+    answer: "9 feet"
+  },
+  {
+    question: "What is a decent way to gauge if you have the ladder at the correct angle? (Page 21)",
+    options: ["Look at it from the side and form an 'L' with your fingers", "Begin climbing and go by feel", "Stand at the base of the ladder and make sure your hands can reach it if holding them straight out", "Ask someone if it looks good"],
+    answer: "Stand at the base of the ladder and make sure your hands can reach it if holding them straight out"
+  },
+  {
+    question: "Stepladders should be nore more than _________ below the height you want to work at. (Page 21)",
+    options: ["1 foot", "2 feet", "3 feet", "4 feet"],
+    answer: "4 feet"
+  },
+  {
+    question: "You should never work of the top 2 rungs or the top of the ladder. (Page 21)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "A general rule of thumb suggests that the ladder is _________ to _________ longer than necessary to reach the highest support point. (Page 21)",
+    options: ["3 to 5 feet", "4 to 7 feet", "7 to 10 feet", "10 to 15 feet"],
+    answer: "7 to 10 feet"
+  },
+  {
+    question: "Twisting on a ladder does not put you in danger of falling. (Page 22)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "You should always try to secure your ladder at the point of contact at the top of the ladder, especially when working in windy conditions. (Page 22)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "You should always use EXTREME CAUTION when using aluminium ladders near power lines because _________. (Page 22)",
+    options: ["It can mean instant death if contacted with a power line", "Aluminium ladders conduct electricity", "Electric can jump from the power line", "All the above"],
+    answer: "All the above"
+  },
+  {
+    question: "Ladders should be kept at a minimum of _________ from any power line. (Page 22)",
+    options: ["5 feet", "10 feet", "15 feet", "20 feet"],
+    answer: "10 feet"
+  },
+  {
+    question: "Decks usually provide adequate traction for ladders. (Page 22)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "When moving up or down a ladder, you should always face the ladder. (Page 23)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "_________ must be provided on stepladders to hold the front and back sections in an open position when ladders are being used. (Page 23)",
+    options: ["A person manually holding the ladder", "Metal spreaders or locking devices", "Ratchet Straps", "2x4s"],
+    answer: "Metal spreaders or locking devices"
+  },
+  {
+    question: "Ladders with structural defect, such as broken or missing rungs, cleats, or steps, broken or split rails; corroded components; or other faulty or defective components, do not have to immediately be marked as defective. (Page 23)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "Not every roofing material is slick when wet or damp. (Page 24)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "These roofing materials are prone to moss and other growths. In addition, stepping on this moss can result in a slip. (Page 24)",
+    options: ["Wood Shakes Only", "Asphalt Shingles Only", "Metal Roofs", "Both Wood Shakes and Asphalt Shingles"],
+    answer: "Both Wood Shakes and Asphalt Shingles"
+  },
+  {
+    question: "What makes Wood Shakes, Slate, and Tile Roofs a pain? (Page 24)",
+    options: ["They are usually found on cheaper houses", "They are easily breakable", "They are found on extremely tall houses", "You need a permit to be able to even step on them"],
+    answer: "They are easily breakable"
+  },
+  {
+    question: "You should not wear slick soled shoes because they do not provide good traction on roofs. (Page 24)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "What point of the roof is stronger and easier to climb? (Page 24)",
+    options: ["The side that usually faces the wind", "On the porch overhang", "On the very edge", "At the ridge and valleys"],
+    answer: "At the ridge and valleys"
+  },
+  {
+    question: "Warped and sunken areas usually indicate a strong point on the roof? (Page 24)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "A _________ is used to navigate steep roof and also stay off fragile or slippery roofs. This is slid up the roof until the hook passes over the peak, and then it is flipped over. (Page 25)",
+    options: ["Scaffolding", "Stepladder", "Roof Ladder and Ridge Hook", "Extension Ladder"],
+    answer: "Roof Ladder and Ridge Hook"
+  },
+  {
+    question: "It is alright to lean a ladder against a chimney if there are visible signs of deteriation, because after all, it is solid masonry. (Page 25)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "You should always use extreme caution leaning a ladder against a wooden chimney chase because it may not be structurally sound or properly secured. (Page 25)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+
+];
+
+if (!window.quizRegistry) window.quizRegistry = {};
+window.quizRegistry['Chapter 3: Health and Safety at the Job Site'] = essentialsChapter3QuizData;
