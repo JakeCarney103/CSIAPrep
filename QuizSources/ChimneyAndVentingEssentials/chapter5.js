acf3a42ca825fa3322e435e63d7047c493b9076b
Jake Carney <jakecarney103@gmail.com>
2026-01-14 14:43:04 -0500

    Create chapter5.js

---
+++ QuizSources/ChimneyAndVentingEssentials/chapter5.js
@@ -0,0 +1,75 @@
+const essentialsChapter5QuizData = [
+  {
+    question: "Firewood, pellets and coal are all _________ fuels? (Page 57)",
+    options: ["Solid", "Hard", "Gas", "Liquid"],
+    answer: "Solid"
+  },
+  {
+    question: "It is the wood type, not the burning habits of the owner, that have more influence on the amount of creosote deposits found. (Page 57)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "When it comes to wood pound for pound, _________ has an identical number of available BTUs. (Page 57)",
+    options: ["All wood", "Not any wood", "Only certain Hardwoods", "Only certain Softwoods"],
+    answer: "All wood"
+  },
+  {
+    question: "It takes _________ pounds of SOFTWOOD to have the same amount of BTUs as 5 pounds of HARDWOOD. (Page 57)",
+    options: ["5", "10", "15", "20"],
+    answer: "5"
+  },
+  {
+    question: "It is important to allow all wood, hard or soft, to burn completely. (Page 57)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "_________ of the wood is a major factor influencing the amount of creosote produced. (Page 58)",
+    options: ["Moisture Content", "Type", "Weight", "Size"],
+    answer: "Moisture Content"
+  },
+  {
+    question: "If you are burning wood, you should want the wood's moisture content to be between _________ and _________. (Page 58)",
+    options: ["15% and 25%", "15% and 30%", "10% and 30%", "0% and 5%"],
+    answer: "15% and 25%"
+  },
+  {
+    question: "Why would wet wood increase creosote deposits? (Page 58)",
+    options: ["The fire is cooler and therefore the flue gases are cooler", "Drying of the wood increases the actual fire's heat", "The flue gases get hotter", "It increases draft"],
+    answer: "The fire is cooler and therefore the flue gases are cooler"
+  },
+  {
+    question: "You can use a _________ on the wood stovepipe to monitor proper flue temperatures so you can maintain an efficient, clean-burning fire. (Page 58)",
+    options: ["Magnetic Thermometer", "Piece of Paper", "Your Hand", "Heat Reclaimer"],
+    answer: "Magnetic Thermometer"
+  },
+  {
+    question: "These are solid fuel, just like cordwood, because they are made from wood by compressing sawdust into small, easy-to-use fuel. (Page 58)",
+    options: ["Wood Pellets", "Gas Logs", "PCRs", "Paper"],
+    answer: "Wood Pellets"
+  },
+  {
+    question: "Due to the manufacturing process of wood pellets, they tend to have a hire moisture content than the average cordwood. (Page 58)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "A PFI Premium pellet has less than _________% moisture content. (Page 59)",
+    options: ["8", "2", "5", "3"],
+    answer: "8"
+  },
+  {
+    question: "A PFI Standard pellet has less than _________% moisture content. (Page 59)",
+    options: ["8", "10", "5", "9"],
+    answer: "10"
+  },
+  {
+    question: "A Utility grade pellet has less than _________% moisture content. (Page 59)",
+    options: ["8", "10", "5", "9"],
+    answer: "10"
+  },
+];
+
+if (!window.quizRegistry) window.quizRegistry = {};
+window.quizRegistry['Chapter 5: Basic Theories'] = essentialsChapter5QuizData;
