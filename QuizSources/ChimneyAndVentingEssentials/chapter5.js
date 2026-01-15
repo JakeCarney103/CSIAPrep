641d64792e488485a58b213a30dee154174fcf41
Jake Carney <jakecarney103@gmail.com>
2026-01-14 20:31:56 -0500

    Stopping near page 65

--- QuizSources/ChimneyAndVentingEssentials/chapter5.js
+++ QuizSources/ChimneyAndVentingEssentials/chapter5.js
@@ -69,6 +69,256 @@ const essentialsChapter5QuizData = [
     options: ["8", "10", "5", "9"],
     answer: "10"
   },
+  {
+    question: "A Utility grade pellet has less than _________% moisture content. (Page 59)",
+    options: ["8", "10", "5", "9"],
+    answer: "10"
+  },
+  {
+    question: "Which appliance type has a more consistent heat output? (Page 59)",
+    options: ["Pellet Appliance", "Cordwood Appliance"],
+    answer: "Pellet Appliance"
+  },
+  {
+    question: "Which appliance type has a more consistent heat output? (Page 59)",
+    options: ["Pellet Appliance", "Gas Appliance"],
+    answer: "Pellet Appliance"
+  },
+  {
+    question: "Which appliance type has a more consistent heat output? (Page 59)",
+    options: ["Pellet Appliance", "Cordwood Appliance", "Gas Appliance"],
+    answer: "Pellet Appliance"
+  },
+  {
+    question: "This type of coal is considered 'hard.' (Page 59)",
+    options: ["Lump", "Anthracite", "Bituminous", "Stoker"],
+    answer: "Anthracite"
+  },
+  {
+    question: "This type of coal is considered 'soft'. (Page 59)",
+    options: ["Lump", "Anthracite", "Bituminous", "Stoker"],
+    answer: "Bituminous"
+  },
+  {
+    question: "Coal is categorized by _________. (Page 59)",
+    options: ["Size", "Value", "Composition", "Level of Burning Cleanliness"],
+    answer: "Size"
+  },
+  {
+    question: "_________ coal comes in sizes called lump, egg, nut, stoker and slack. (Page 59)",
+    options: ["Anthracite", "Bituminous", "Cheap", "Rare"],
+    answer: "Bituminous"
+  },
+  {
+    question: "_________ coal comes in sizes called egg, stove, chestnut, pea, nut and buckwheat. (Page 59)",
+    options: ["Anthracite", "Bituminous", "Cheap", "Rare"],
+    answer: "Anthracite"
+  },
+  {
+    question: "Coal contains roughly _________ times more BTUs per pound than cordwood. (Page 59)",
+    options: ["2.5", "2", "5", "1.5"],
+    answer: "2.5"
+  },
+  {
+    question: "Burning coal in a stove that is only designed to burn wood will result in _________. (Page 59)",
+    options: ["Significant damage to the internal parts of the firebox", "A cooler fire due to the wrong design", "Minimized risk of fire extending into the building structure", "Savings and less maintenance for the customer"],
+    answer: "Significant damage to the internal parts of the firebox"
+  },
+  {
+    question: "Burning coal requires a _________ since the primary combustion air must come from below the fuel. (Page 59)",
+    options: ["Grate", "Fan", "Direct contact to the bottom of the firebox", "Secondary fire"],
+    answer: "Grate"
+  },
+  {
+    question: "Primary combustion air must come from _________ when it comes to burning coal. (Page 59)",
+    options: ["Below the fuel", "Above the fuel", "A fan", "The flue gases"],
+    answer: "Below the fuel"
+  },
+  {
+    question: "The residue from _________ burning is often a dark soot that can be very lightweight. (Page 59)",
+    options: ["Gas", "Wood", "Coal", "Pellets"],
+    answer: "Coal"
+  },
+  {
+    question: "Gas hearth appliances produce inconsistent heat. (Page 59)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "Natural Gas produces _________ BTUs. (Page 60)",
+    options: ["1000", "2000", "2500", "8200"],
+    answer: "1000"
+  },
+  {
+    question: "Natural Gas is heavier than air. (Page 60)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "Natural Gas requires about  _________ cubic feet of air for every cubic foot of gas to burn efficiently. (Page 60)",
+    options: ["10", "24", "20", "30"],
+    answer: "10"
+  },
+  {
+    question: "Liquefied petroleum is commonly known as _________. (Page 60)",
+    options: ["LP", "LPG", "Propane", "All of these"],
+    answer: "All of these"
+  },
+  {
+    question: "LP produces _________ BTUs. (Page 60)",
+    options: ["1000", "2000", "2500", "8200"],
+    answer: "2500"
+  },
+  {
+    question: "LP is heavier than air. (Page 60)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "LP requires about  _________ cubic feet of air for every cubic foot of gas to burn efficiently. (Page 60)",
+    options: ["10", "24", "20", "30"],
+    answer: "24"
+  },
+  {
+    question: "If there is a Natural Gas leak in the house, where would the gas travel to? (Page 60)",
+    options: ["The lowest part of the house", "The warmest part of the house", "The highest part of the house", "The coldest part of the house"],
+    answer: "The highest part of the house"
+  },
+  {
+    question: "If there is an LP leak in the house, where would the gas travel to? (Page 60)",
+    options: ["The lowest part of the house", "The warmest part of the house", "The highest part of the house", "The coldest part of the house"],
+    answer: "The lowest part of the house"
+  },
+  {
+    question: "When burning gas, for every _________ BTUs produced, _________ gallon of water vapor is produced. (Page 61)",
+    options: [
+				"For every 100,000 BTUs produced, 1 gallon of water vapor is produced", 
+				"For every 200,000 BTUs produced, 1 gallon of water vapor is produced", 
+				"For every 10,000 BTUs produced, 1 gallon of water vapor is produced",
+				"For every 100,000 BTUs produced, 2 gallons of water vapor is produced"
+			 ],
+    answer: "For every 100,000 BTUs produced, 1 gallon of water vapor is produced"
+  },
+  {
+    question: "When it comes to burning gas, oversized chimneys cause an issue because more condensation can form due to lower flue gas temperatures. (Page 61)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "For gas appliances, as the temperature of flue gases decrease, the possibility of condensation in the flue _________. (Page 61)",
+    options: ["Increases", "Decreases", "Stays the same", "Becomes impossible"],
+    answer: "Increases"
+  },
+  {
+    question: "For gas appliances, the higher the efficiency, the lower the temperature of the _________. (Page 61)",
+    options: ["Flue Gases", "BTU Output", "Inside Air", "Appliance"],
+    answer: "Flue Gases"
+  },
+  {
+    question: "The primary source of fuel in the northeast portion of the country is _________. (Page 61)",
+    options: ["Gas", "Wood", "Fuel Oil", "Coal"],
+    answer: "Fuel Oil"
+  },
+  {
+    question: "Oil-fired appliances may be listed for use with _________ or vented into a traditional masonry or factory-built chimney. (Page 61)",
+    options: ["B-Vent", "BW-Vent", "L-Vent", "No other type of vent"],
+    answer: "L-Vent"
+  },
+  {
+    question: "Due to the lower stack temperatures, high-efficiency fuel oil appliances may produce a lot of _________. (Page 61)",
+    options: ["Carbon Monoxide", "Water Vapor", "Back Draft at rest", "Back Puffing"],
+    answer: "Water Vapor"
+  },
+  {
+    question: "Oil-fired appliances installed into oversized chimneys may cause _________. (Page 61)",
+    options: ["Premature deterioration of the masonry flue liner and unlined chimneys", "Stack Effect", "3rd Degree Creosote", "Complete Appliance Failure"],
+    answer: "Premature deterioration of the masonry flue liner and unlined chimneys"
+  },
+  {
+    question: "Oil soot is very light and should be cleaned with _________. (Page 61)",
+    options: ["A soft bristle brush capable of 'swabbing' the flue", "Round Wire Brushes", "Rotary Tools with Chains", "Chemicals"],
+    answer: "A soft bristle brush capable of 'swabbing' the flue"
+  },
+  {
+    question: "What may happen to Round Wire Brushes if used to clean a flue venting an oil-fired appliance? (Page 61)",
+    options: ["Deteriorate because of the residual sulfur in the flue", "Expand in size and get stuck", "Perform an excellent job of brushing the soot", "The bristles will stick together due to moisture and soot"],
+    answer: "Deteriorate because of the residual sulfur in the flue"
+  },
+  {
+    question: "Combustion is the oxidation of fuel resulting in the release of energy, usually in the form of _________. (Page 62)",
+    options: ["Heat and Light", "Heat and Smell", "Oxygen and Fuel", "Smoke and Creosote"],
+    answer: "Heat and Light"
+  },
+  {
+    question: "These 3 elements are required for combustion. (Page 62)",
+    options: ["Heat, Oxygen, and Fuel", "Smoke, Gas, and Fuel", "Pyrolysis, Charcoal, and Gas", "Wood, Fireplace, and Flue"],
+    answer: "Heat, Oxygen, and Fuel"
+  },
+  {
+    question: "Incomplete combustion or complete stopping could happen to burning if heat, oxygen, and fuel are present in the wrong proportion. (Page 62)",
+    options: ["Heat, Oxygen, and Fuel", "Smoke, Gas, and Fuel", "Pyrolysis, Charcoal, and Gas", "Wood, Fireplace, and Flue"],
+    answer: "Heat, Oxygen, and Fuel"
+  },
+  {
+    question: "Smoke is _________. (Page 62)",
+    options: ["Unburned hydrocarbons", "Water Vapor", "Necessary in the flue", "Proof fuel is burning efficiently"],
+    answer: "Unburned hydrocarbons"
+  },
+  {
+    question: "Smoke that condenses in the venting system is called _________. (Page 62)",
+    options: ["Creosote", "Water Vapor", "Histoplasmosis", "Arsenic"],
+    answer: "Creosote"
+  },
+  {
+    question: "Fuel, Heat, Oxygen, and Chemical Chain Reaction are the 4 elements of the _________. (Page 63)",
+    options: ["Fire Tetrahedron", "Fire Triangle", "Combustion Square", "Fire Cube"],
+    answer: "Fire Tetrahedron"
+  },
+  {
+    question: "How many stages of wood combustion are there? (Page 64)",
+    options: ["2", "3", "4", "5"],
+    answer: "4"
+  },
+  {
+    question: "What is the first stage of wood combustion? (Page 64)",
+    options: ["Moisture Evaporation (Drying)", "Ignition of hydrocarbon gases (Burning)", "Vaporization of hydrocarbon compounds (Pyrolysis)", "Char burning (Charcoal)"],
+    answer: "Moisture Evaporation (Drying)"
+  },
+  {
+    question: "What is the second stage of wood combustion? (Page 64)",
+    options: ["Moisture Evaporation (Drying)", "Ignition of hydrocarbon gases (Burning)", "Vaporization of hydrocarbon compounds (Pyrolysis)", "Char burning (Charcoal)"],
+    answer: "Vaporization of hydrocarbon compounds (Pyrolysis)"
+  },
+  {
+    question: "What is the third stage of wood combustion? (Page 64)",
+    options: ["Moisture Evaporation (Drying)", "Ignition of hydrocarbon gases (Burning)", "Vaporization of hydrocarbon compounds (Pyrolysis)", "Char burning (Charcoal)"],
+    answer: "Ignition of hydrocarbon gases (Burning)"
+  },
+  {
+    question: "What is the fourth stage of wood combustion? (Page 64)",
+    options: ["Moisture Evaporation (Drying)", "Ignition of hydrocarbon gases (Burning)", "Vaporization of hydrocarbon compounds (Pyrolysis)", "Char burning (Charcoal)"],
+    answer: "Char burning (Charcoal)"
+  },
+  {
+    question: "What is the first stage of wood combustion? (Page 64)",
+    options: ["Moisture Evaporation (Drying)", "Ignition of hydrocarbon gases (Burning)", "Vaporization of hydrocarbon compounds (Pyrolysis)", "Char burning (Charcoal)"],
+    answer: "Moisture Evaporation (Drying)"
+  },
+  {
+    question: "What is the second stage of wood combustion? (Page 64)",
+    options: ["Moisture Evaporation (Drying)", "Ignition of hydrocarbon gases (Burning)", "Vaporization of hydrocarbon compounds (Pyrolysis)", "Char burning (Charcoal)"],
+    answer: "Vaporization of hydrocarbon compounds (Pyrolysis)"
+  },
+  {
+    question: "What is the third stage of wood combustion? (Page 64)",
+    options: ["Moisture Evaporation (Drying)", "Ignition of hydrocarbon gases (Burning)", "Vaporization of hydrocarbon compounds (Pyrolysis)", "Char burning (Charcoal)"],
+    answer: "Ignition of hydrocarbon gases (Burning)"
+  },
+  {
+    question: "_________ is a chemical alteration of the fuel molecules due to the application of heat? (Page 64)",
+    options: ["Pyrolysis", "Oxidation", "Condensation", "Moisture evaporation (Drying)"],
+    answer: "Pyrolysis"
+  },
 ];
 
 if (!window.quizRegistry) window.quizRegistry = {};
