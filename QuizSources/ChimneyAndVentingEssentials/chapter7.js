765952ce6b0a0fda47595966f5e0d4d57dea314c
Jake Carney <jakecarney103@gmail.com>
2026-01-17 16:53:05 -0500

    Leave off at page 100

--- QuizSources/ChimneyAndVentingEssentials/chapter7.js
+++ QuizSources/ChimneyAndVentingEssentials/chapter7.js
@@ -150,7 +150,7 @@ const essentialsChapter7QuizData = [
     answer: "False",
   },
   {
-    question: "Coating and repair systems repair joints in tile liners and resurface defecting flue tiles with refractory materials. (Page 92)",
+    question: "Coating and repair systems repair joints in tile liners and resurface defecting flue tiles with refractory materials. (Page 93)",
     options: ["True", "False"],
     answer: "True",
   },
@@ -158,6 +158,256 @@ const essentialsChapter7QuizData = [
     question: "Double wall connector pipe can be cut to size. (Page 92)",
     options: ["True", "False"],
     answer: "False",
+  },
+  {
+    question: "Stainless steel lining systems come in 2 major categories; _________. (Page 94)",
+    options: ["Rigid and Flexible", "Long and Short", "Sharp and Smooth", "Refractory and General"],
+    answer: "Rigid and Flexible",
+  },
+  {
+    question: "_________ is defined as a mix of different metals or a mix of an element and a metal. (Page 94)",
+    options: ["Alloy", "Catalyst", "Steel", "Refractory"],
+    answer: "Alloy",
+  },
+  {
+    question: "_________ These alloys are best suited for wood and pellet burning applications and still being used. They are not recommended in gas, coal or oil burning situations. (Page 94)",
+    options: ["304 and 304L", "AL29-4C", "316, 316L and 316TI", "321"],
+    answer: "304 and 304L",
+  },
+  {
+    question: "_________ These alloys are often termed all fuel alloys (wood, pellet, coal, oil and gas). When it comes to gas, they are only suitable for Category 1. (Page 94)",
+    options: ["304 and 304L", "AL29-4C", "316, 316L and 316TI", "321"],
+    answer: "316,316L and 316TI",
+  },
+  {
+    question: "_________ These alloys are suitable for wood and pellet burning applications. However, it is no longer being used. (Page 94)",
+    options: ["304 and 304L", "AL29-4C", "316, 316L and 316TI", "321"],
+    answer: "321",
+  },
+  {
+    question: "_________ These alloys are solely used for venting gas appliances and , in some cases, bio fuel appliances. (Page 94)",
+    options: ["304 and 304L", "AL29-4C", "316, 316L and 316TI", "321"],
+    answer: "AL29-4C",
+  },
+  {
+    question: "304 alloys are recommended for venting Gas appliances. (Page 94)",
+    options: ["True", "False"],
+    answer: "False",
+  },
+  {
+    question: "Most rigid liner sections are made of series 321 stainless steel. (Page 94)",
+    options: ["True", "False"],
+    answer: "False",
+  },
+  {
+    question: "Most rigid liner sections are typically 1-4 feet long. (Page 94)",
+    options: ["True", "False"],
+    answer: "True",
+  },
+  {
+    question: "What typically secures the joints on a rigid liner? (Page 94)",
+    options: ["Stainless steel pop rivets", "Stainless steel screws", "Twist-lock fittings", "Any of these"],
+    answer: "Any of these",
+  },
+  {
+    question: "Rigid liners are supported at the top of the chimney or the thimble area. (Page 94)",
+    options: ["True", "False"],
+    answer: "True",
+  },
+  {
+    question: "Rigid stainless-steel liners cannot expand vertically during the heating of the liner. (Page 94)",
+    options: ["True", "False"],
+    answer: "False",
+  },
+  {
+    question: "_________ are required for use on chimneys with stainless steel liners. (Page 94)",
+    options: ["Rain Caps", "Washes", "Hearth Stoves", "Top Dampers"],
+    answer: "Rain Caps",
+  },
+  {
+    question: "All flexible stainless-steel liners are supplied in 2 ft sections. (Page 94)",
+    options: ["True", "False"],
+    answer: "False",
+  },
+  {
+    question: "This liner is installed as one continuous piece. (Page 94)",
+    options: ["Flexible Liners", "Rigid Liners", "Stovepipe", "Manufactured Chimneys"],
+    answer: "Flexible Liners",
+  },
+  {
+    question: "Both Rigid and Flexible liners require Rain Caps after installation. (Page 94)",
+    options: ["True", "False"],
+    answer: "True",
+  },
+  {
+    question: "To enhance performance and reduce potential hazards, chimney liners are _________. (Page 95)",
+    options: ["Wrapped in insulation", "Made of titanium", "Used only with open fireplaces", "Exposed directly to the outside"],
+    answer: "Wrapped in insulation",
+  },
+  {
+    question: "How does an insulated liner reduce creosoted and establish draft quicker? (Page 95)",
+    options: ["Keeps the liner and the inner flue gases warmer", "Does not let outside gases mix with the flue gases", "Slows the flow of gases", "Decreases the pressure difference between the outside and inside"],
+    answer: "Keeps the liner and the inner flue gases warmer",
+  },
+  {
+    question: "Insulated pellet, oil and gas flue liners do not improve these systems. They only improve wood burning systems. (Page 95)",
+    options: ["True", "False"],
+    answer: "False",
+  },
+  {
+    question: "One common method of insulating a stainless steel liner is wrapping it with a blanket of _________. (Page 95)",
+    options: ["Ceramic Wool", "Fiberglass Insulation", "Aluminum Foil", "Horse Hair Insulation"],
+    answer: "Ceramic Wool",
+  },
+  {
+    question: "A common way of insulating a stainless steel liner is to fill the void between the liner and the chimney walls with _________. (Page 95)",
+    options: ["A mixture of expanded mica, perlite or vermiculite and cement", "Fiberglass Insulation", "Expanding Foam", "Refractory Silicone"],
+    answer: "A mixture of expanded mica, perlite, or vermiculite and cement",
+  },
+  {
+    question: "A common way of insulating a stainless steel liner is to use pre-insulated liners that utilize a ceramic blanket and an outer layer of _________ to secure the insulation. (Page 95)",
+    options: ["Aluminum or Stainless steel", "Duct Tape", "Fiberglass Insulation", "Refractory cement"],
+    answer: "Aluminum or Stainless steel",
+  },
+  {
+    question: "In the past, stainless steel chimney liners were sometimes insulated with a loose fill of expanded mica, perlite or vermiculite without something. This could result in serious issues. For example, if the liner developed a hole, the loose fill could leak into the liner and block the thimble. What was missing from the mix? (Page 95)",
+    options: ["Cement", "Silicone", "Expanding Foam", "Glue"],
+    answer: "Cement",
+  },
+  {
+    question: "These liner systems are produced in 2 basic methods. One method uses a slurry mixture pumped into a chimney around an inflated bladder, and then after the slurry dries the bladder is deflated; the second uses a slip-form technique that packs a mixture to the chimney wall to create a continuous flue. (Page 96)",
+    options: ["Flexible Liner", "Cast-In-Place Liner", "B-Vent Liner", "Rigid Liner"],
+    answer: "Cast-In-Place Liner",
+  },
+  {
+    question: "The standard for chimney lining and re-lining systems is _________. (Page 96)",
+    options: ["UL 1777", "UL 127", "UL 103", "UL 1482"],
+    answer: "UL 1777",
+  },
+  {
+    question: "If called to sweep an unfamiliar listed liner system, you should _________. (Page 97)",
+    options: ["Ask the homeowner for a copy of the installation information", "Take an educated guess based on the appliance", "Always use a Flat Wire Brush", "Immediately refuse to sweep it"],
+    answer: "Ask the homeowner for a copy of the installation information",
+  },
+  {
+    question: "_________ are a listed venting system made as a double-walled pipe with an air space between the walls. They are used for venting listed Category 1 gas appliances, or those with draft hoods. (Page 97)",
+    options: ["B-Vent", "Direct Vent", "L-Vent", "Specialty Gas Vent"],
+    answer: "B-Vent",
+  },
+  {
+    question: "B-Vent is a single-walled pipe. (Page 97)",
+    options: ["True", "False"],
+    answer: "False",
+  },
+  {
+    question: "B-Vent vents ALL categories of gas appliances. (Page 97)",
+    options: ["True", "False"],
+    answer: "False",
+  },
+  {
+    question: "B-Vent's aluminum inner pipe is designed to withstand appliance outlet temperatures up to about _________°F. (Page 97)",
+    options: ["1000", "480", "840", "500"],
+    answer: "480",
+  },
+  {
+    question: "_________ is a vent designed to be used within a wall to vent a gas appliance that is approved for use with this vent. (Page 97)",
+    options: ["B-Vent", "L-Vent", "BW Vent", "Direct Vent"],
+    answer: "BW Vent",
+  },
+  {
+    question: "Most heath appliances are not approved for use with BW Vent. (Page 97)",
+    options: ["True", "False"],
+    answer: "True",
+  },
+  {
+    question: "_________ units are sealed combustion systems that work by drawing outside air for combustion and expel combustion gases and by products outside the home. (Page 97)",
+    options: ["B-Vent", "BW Vent", "Direct Vent", "L-Vent"],
+    answer: "Direct Vent",
+  },
+  {
+    question: "Today approximately _________ of the new gas hearth appliances use direct vent technology. (Page 97)",
+    options: ["80%", "50%", "90%", "25%"],
+    answer: "80%",
+  },
+  {
+    question: "When using Direct Vent appliances, if the manufacturer specifies a specific brand of pipe you can use a different brand as long as they are similar. (Page 98)",
+    options: ["True", "False"],
+    answer: "False",
+  },
+  {
+    question: "Many new installations require _________ venting systems to vent direct vent appliances. These systems use a pipe inside a pipe. (Page 98)",
+    options: ["Co-axial", "Co-linear", "L-Vent", "B-Vent"],
+    answer: "Co-axial",
+  },
+  {
+    question: "Co-axial direct vents are systems where 2 pipes run side by side. (Page 98)",
+    options: ["True", "False"],
+    answer: "False",
+  },
+  {
+    question: "Direct vent fireplace inserts, most furnaces and boilers, and some factory built fireplaces may use 2 flexible aluminum liners. One is responsible to bring fresh air in, while the other exhausts the flue gases. This venting system is known as _________. (Page 98)",
+    options: ["Co-linear", "Co-axial", "L-Vent", "Power Vent"],
+    answer: "Co-linear",
+  },
+  {
+    question: "Aluminum liners are allowed to be used to vent gas fired appliances as long as they are approved for use with _________. (Page 98)",
+    options: ["B-Vent", "Co-Linear Vent", "Direct Vent", "L-Vent"],
+    answer: "B-Vent",
+  },
+  {
+    question: "In most cases, these are designed for appliances that operate under positive pressure in the venting system. They may be plastic, pvc, or metal and single or double wall. (Page 98)",
+    options: ["B-Vent", "Specialty Gas Vents", "Direct Vent", "L-Vent"],
+    answer: "Specialty Gas Vents",
+  },
+  {
+    question: "_________ are controlled devices with fans placed at the end of the vent pipe to pull the products of combustion out of the venting system (Page 99)",
+    options: ["B-Vent", "Co-Linear Vent", "Co-axial Vent", "Power Vents"],
+    answer: "Power Vents",
+  },
+  {
+    question: "Which of the following is true when it comes to differing L-Vent and B-Vent? (Page 99)",
+    options: ["L-Vent uses Stainless Steel while B-Vent uses Aluminum", "L-Vent uses Aluminum while B-Vent uses Stainless Steel", "L-Vent uses Stainless Steel while B-Vent uses Titanium", "L-Vent uses Aluminum while B-Vent uses PVC"],
+    answer: "L-Vent uses Stainless Steel while B-Vent uses Aluminum",
+  },
+  {
+    question: "Fuel oil and Pellet Appliances use this type of vent. (Page 99)",
+    options: ["B-Vent", "Co-Linear Vent", "Direct Vent", "L-Vent"],
+    answer: "L-Vent",
+  },
+  {
+    question: "L-Vent and Pellet Vent are essentially the same thing. (Page 99)",
+    options: ["True", "False"],
+    answer: "True",
+  },
+  {
+    question: "Most pellet appliances utilize _________ to pressurize the vent. (Page 99)",
+    options: ["A fan", "Natural Draft", "Flow", "Top Damper"],
+    answer: "A fan",
+  },
+  {
+    question: "Pellet Vent pipe joints must contain pressure utilizing a gasket, high-temp sealant, or special design. (Page 99)",
+    options: ["True", "False"],
+    answer: "True",
+  },
+  {
+    question: "L Vent was designed to vent oil burning appliances with flue gas temperatures of _________°F or less and flue gases high in sulfur content. (Page 99)",
+    options: ["480", "570", "1000", "500"],
+    answer: "570",
+  },
+  {
+    question: "A _________ is a plate or mechanism used to close off the venting system when not in use. (Page 99)",
+    options: ["Damper", "Throat", "Baffle Plate", "Firestop"],
+    answer: "Damper",
+  },
+  {
+    question: "You are inspecting a fireplace where the damper is located at the throat. This type of damper is called a _________. (Page 100)",
+    options: ["Throat Damper", "Lintel Damper", "Top Damper", "Firebox Damper"],
+    answer: "Throat Damper",
+  },
+  {
+    question: "Pellet Vent pipe joints must contain pressure utilizing a gasket, high-temp sealant, or special design. (Page 99)",
+    options: ["True", "False"],
+    answer: "True",
   },
 ];
 
