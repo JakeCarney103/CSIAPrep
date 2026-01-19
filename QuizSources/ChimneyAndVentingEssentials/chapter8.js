cafa5f07fa1bcbc97024b894c163fb350c77c318
Jake Carney <jakecarney103@gmail.com>
2026-01-19 10:40:44 -0500

    Update chapter8.js

--- QuizSources/ChimneyAndVentingEssentials/chapter8.js
+++ QuizSources/ChimneyAndVentingEssentials/chapter8.js
@@ -1,8 +1,233 @@
 const essentialsChapter8QuizData = [
   {
-    question: "A chimney or venting system that relies on natural draft will perform best if it is located _________? (Page 119)",
+    question: "A chimney or venting system that relies on natural draft will perform best if it is located _________. (Page 119)",
     options: ["Inside the building envelope and is taller than the highest point of the roof", "Inside the building envelope and is shorter than the highest point of the roof", "Outside the building envelope and is taller than the highest point of the roof", "Outside the building envelope and is shorter than the highest point of the roof"],
     answer: "Inside the building envelope and is taller than the highest point of the roof"
+  },
+  {
+    question: "This is best used for dust control. (Page 119)",
+    options: ["Turning your vacuum control", "Sweeping a chimney with large flow", "Letting draft do all the work", "Opening a window so the dust can escape quickly before it settles"],
+    answer: "Inside the building envelope and is taller than the highest point of the roof"
+  },
+  {
+    question: "Prior to doing any work, you should set up drop cloths, vacuum equipment, and any other required materialst to prevent any soot or dust from entering the living quarters or soiling the area around the fireplace. (Page 119)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "When examining the log grate, it should not be _________. (Page 120)",
+    options: ["Excessively warped or worn", "Cast iron", "Covered in ash", "Discolored"],
+    answer: "Excessively warped or worn"
+  },
+  {
+    question: "When examining the log grate, it should not be _________. (Page 120)",
+    options: ["Excessively warped or worn", "Cast iron", "Covered in ash", "Discolored"],
+    answer: "Excessively warped or worn"
+  },
+  {
+    question: "A discolored fireplace facade may be evidence of _________. (Page 120)",
+    options: ["A smoking problem or marginal draft signifying a performance issue", "Proper use of the fireplace", "A well functioning system", "Creosote leaking through the bricks"],
+    answer: "A smoking problem or marginal draft signifying a performance issue"
+  },
+  {
+    question: "When examining the damper, it should not be _________. (Page 120)",
+    options: ["Warped", "Soot covered", "Functioning", "Open"],
+    answer: "Warped"
+  },
+  {
+    question: "If you find creosote build-up in the smoke chamber, you should _________. (Page 120)",
+    options: ["Sweep the entire chimney", "Call 911", "Immediately inform the customer that their system needs completely replaced", "Check the flue tile/liner to determine if it needs swept"],
+    answer: "Sweep the entire chimney"
+  },
+  {
+    question: "_________ is the standard of care for fireplace inspections. (Page 120)",
+    options: ["IRC", "NFPA 211", "NFPA 54", "Chimney Venting and Essentials Book"],
+    answer: "NFPA 211"
+  },
+  {
+    question: "When inspecting metal fireboxes, if there are cracks, holes or other disfiguration of the steel box or seams, the unit is still safe for use. (Page 121)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "When metal fireboxes are deteriorated _________. (Page 121)",
+    options: ["Liquid Weld should be applied to holes", "Fires should be kept very small", "Replacement should be considered", "Normal use is still fine"],
+    answer: "Replacement should be considered"
+  },
+  {
+    question: "The level of inspection and all facets of the inspection should always be documented and completed. (Page 121)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "If you suspect conditions or issues that are not visible, you should _________. (Page 121)",
+    options: ["Recommend a higher level of inspection to verify", "Keep it to yourself", "Immediately break off any stone facing to see", "Charge them more money"],
+    answer: "Recommend a higher level of inspection to verify"
+  },
+  {
+    question: "All recommendations should be _________. (Page 121)",
+    options: ["Documented in a written format", "Only brought up in passing verbally", "Performed without the homeowners permission", "Kept from the customer"],
+    answer: "Documented in a written format"
+  },
+  {
+    question: "One of the first things to check when inspecting an appliance in the home is _________. (Page 121)",
+    options: ["The clearances to visible and accessible combustibles", "The condition of the crown", "The thickness of the smoke chamber walls", "The condition of the morter joints inbetween flue tiles"],
+    answer: "The clearances to visible and accessible combustibles"
+  },
+  {
+    question: "Clearances are the maximum distance the appliance should be placed from combustibles. (Page 121)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "When the installation manual is unavailable and no data plate exists or is readable, we should assume the appliance is _________. (Page 122)",
+    options: ["Safe", "Faulty", "Listed", "Unlisted"],
+    answer: "Unlisted"
+  },
+  {
+    question: "The clearances for an unlisted appliance are _________ to combustibles. (Page 122)",
+    options: ["36 inches", "18 inches", "12 inches", "48 inches"],
+    answer: "36 inches"
+  },
+  {
+    question: "The connector pipe that runs from the appliance flue collar to the chimney flue should be as long as possible. (Page 122)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "How does smoke want to travel? (Page 122)",
+    options: ["Vertically in a spiral", "Horizontally", "Down the chimney", "To the hottest point of the room"],
+    answer: "Vertically in a spiral"
+  },
+  {
+    question: "According to the IRC, the amount of horizontal run of a connector pipe cannot be more than _________ of the vertical rise. (Page 122)",
+    options: ["50%", "75%", "100%", "40%"],
+    answer: "75%"
+  },
+  {
+    question: "According to the NFPA 211, the amount of horizontal run of a connector pipe cannot be more than _________ of the vertical rise. (Page 122)",
+    options: ["50%", "75%", "100%", "40%"],
+    answer: "50%"
+  },
+  {
+    question: "The default clearance to single-wall pipe is _________. (Page 122)",
+    options: ["18 inches", "36 inches", "9 inches", "6 inches"],
+    answer: "18 inches"
+  },
+  {
+    question: "Using clearance reductin methods, what is the lowest you can bring the clearance of single-wall pipe to? (Page 122)",
+    options: ["18 inches", "12 inches", "9 inches", "6 inches"],
+    answer: "6 inches"
+  },
+  {
+    question: "Where connector pipe joins, there should be _________ screws. (Page 122)",
+    options: ["3", "5", "4", "2"],
+    answer: "3"
+  },
+  {
+    question: "Horizontal single-wall stovepipe should be installed with the seams _________? (Page 122)",
+    options: ["Up", "Down", "Towards the wall", "Away from the wall"],
+    answer: "Up"
+  },
+  {
+    question: "When joining connector pipe for a wood burning appliance, the male end of the connector pipe should always be TOWARDS THE APPLIANCE. (Page 122)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "When joining connector pipe for a gas or oil burning appliance, the male end of the connector pipe should always be TOWARDS THE APPLIANCE. (Page 122)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "The use of a double wall connector will INCREASE the amount of the heat that radiates from the connector pipe into the room. (Page 123)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "_________ indicates the amount of heat that will flow through one square foot of a uniform material one-inch thick in one hour. (Page 123)",
+    options: ["Thermal Conductivity (k value)", "Thermal Conductivity (R value)", "Thermal Resistance (k value)", "Thermal Resistance (R value)"],
+    answer: "Thermal Conductivity (k value)"
+  },
+  {
+    question: "The higher the k value (Thermal Conductivity), the more heat being conducted through the non-combustible material to the combustible material beneath it. (Page 123)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "_________ is a measure of a material's resistance to heat transfer. (Page 123)",
+    options: ["Thermal Conductivity (k value)", "Thermal Conductivity (R value)", "Thermal Resistance (k value)", "Thermal Resistance (R value)"],
+    answer: "Thermal Resistance (R value)"
+  },
+  {
+    question: "The higher the R value (Thermal Resistance), the MORE heat transfered to the combustible material around/beneath it. (Page 123)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "You can add the R-value of several materials and caculate a total. (Page 123)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "You can add the k-value of several materials and caculate a total. (Page 123)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "If multiple materials have different k values, how can you determine the appropriate thickness and materials. (Page 123)",
+    options: ["Convert the k values to R and then add the R values", "Subtract the k values from the R values", "Subtract the R values from the k values", "Add the k values together"],
+    answer: "Convert the k values to R and then add the R values"
+  },
+  {
+    question: "UL 1618, Standard for Safet;y Wall Protectors, Floor Protectors, and HEarth extenstions, madates that the thermal requirements of manufactured hearth boards should be specified in _________. (Page 123)",
+    options: ["BTUs", "R Values", "k Values", "°F"],
+    answer: "R Values"
+  },
+  {
+    question: "A UL 1618 Type 1 board provides how much thermal protection? (Page 123)",
+    options: ["None", "R = 5.0", "R = 1.0", "R = 2.5"],
+    answer: "None"
+  },
+  {
+    question: "A UL 1618 Type 1 board provides how much thermal protection? (Page 123)",
+    options: ["R >= 1.0", "R = 0.5", "R <= 1.0", "None"],
+    answer: "R >= 1.0"
+  },
+  {
+    question: "Where can you find the a UL 1618 Hearth Board's information? (Page 123)",
+    options: ["Under it", "On the top left corner", "On the side", "On the appliance's data plate"],
+    answer: "Under it"
+  },
+  {
+    question: "Unlisted appliances require _________ of floor protection on all sides. (Page 123)",
+    options: ["18 inches", "36 inches", "12 inches", "9 inches"],
+    answer: "18 inches"
+  },
+  {
+    question: "If embers are buried in ash, they usually stay hot for a maximum of 1 hour. (Page 124)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "When it comes to seams and firebox walls on steel, cast iron or soapstone appliances, ALL seams must be sealed. (Page 124)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "If cracked or warped inner parts are found in an appliance, it often means _________. (Page 124)",
+    options: ["The appliance has been over-fired", "The appliance has water damage", "Wood has been thrown harshly into the appliance", "Massively Incomplete Combustion has occurred"],
+    answer: "The appliance has been over-fired"
+  },
+  {
+    question: "The purpose of the firebrick is to _________. (Page 124)",
+    options: ["Only to protect the firebox wall", "Only to Reflect Heat", "Only to keep heat inside the firebox", "To protect the firebox wall and keep heat inside the firebox"],
+    answer: "To protect the firebox wall and keep heat inside the firebox"
+  },
+  {
+    question: "Secondary Air Tubes are responsible for _________. (Page 124)",
+    options: ["Bringing in air to burn gases before they exit", "Dispersing air to the outside", "Lowering the ignition temperature of gases", "Being extra parts for the main air intake"],
+    answer: "Bringing in air to burn gases before they exit"
   },
 ];
 
