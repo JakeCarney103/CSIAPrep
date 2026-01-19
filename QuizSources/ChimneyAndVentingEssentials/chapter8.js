e999751eb85919400a931855ada77acc396f9bd1
Jake Carney <jakecarney103@gmail.com>
2026-01-19 08:54:53 -0500

    Create chapter8.js

---
+++ QuizSources/ChimneyAndVentingEssentials/chapter8.js
@@ -0,0 +1,10 @@
+const essentialsChapter8QuizData = [
+  {
+    question: "A chimney or venting system that relies on natural draft will perform best if it is located _________? (Page 119)",
+    options: ["Inside the building envelope and is taller than the highest point of the roof", "Inside the building envelope and is shorter than the highest point of the roof", "Outside the building envelope and is taller than the highest point of the roof", "Outside the building envelope and is shorter than the highest point of the roof"],
+    answer: "Inside the building envelope and is taller than the highest point of the roof"
+  },
+];
+
+if (!window.quizRegistry) window.quizRegistry = {};
+window.quizRegistry['Chapter 8: Inspecting the Appliance'] = essentialsChapter8QuizData;
