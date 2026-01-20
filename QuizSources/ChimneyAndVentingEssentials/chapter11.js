665c7f28b01809992e961c0cbbfcab1e66001d6e
Jake Carney <jakecarney103@gmail.com>
2026-01-20 13:10:13 -0500

    Create chapter11.js

---
+++ QuizSources/ChimneyAndVentingEssentials/chapter11.js
@@ -0,0 +1,29 @@
+const essentialsChapter11QuizData = [
+  {
+    question: "One of the last things you should do before sweeping is roll out drop cloths, runners or other covering from the entry point of the home to your work area. (Page 147)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "To guard against exposure to creosote and soot, what should you do? (Page 147)",
+    options: ["Wear a helmet", "Wear all related PPE", "Turn on a fan", "Hold your breath"],
+    answer: "Wear all related PPE"
+  },
+  {
+    question: "As you being your examinination with the firebox, what should you do if the screen is easily removable? (Page 147)",
+    options: ["Remove it and lean it up against the wall", "Remove it and sit it inside the firebox", "Remove it and sit it on a drop cloth so it doesn't get knocked over", "You should never remove one of those"],
+    answer: "Remove it and sit it on a drop cloth so it doesn't get knocked over"
+  },
+  {
+    question: "As you are examining the log grate, if you notice it is excessively warped or worn, it is alright. (Page 147)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "The log grate should be nore more than _________ the firebox width and _________ the firebox depth. (Page 147)",
+    options: ["1/2 the width and 1/2 the depth", "2/3 the width and 1/2-2/3 the depth", "1/2-2/3 the width and 1/2 the depth", "1/4-1/2 the width and 1/4-1/2 the depth"],
+    answer: "2/3 the width and 1/2-2/3 the depth"
+];
+
+if (!window.quizRegistry) window.quizRegistry = {};
+window.quizRegistry['Chapter 11: Sweeping Masonry Fireplace Chimneys'] = essentialsChapter11QuizData;
