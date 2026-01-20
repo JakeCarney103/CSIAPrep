6bb166a2fb4e4a9237fe2ffa367f4337f33d7e9f
Jake Carney <jakecarney103@gmail.com>
2026-01-20 12:56:03 -0500

    Finish Chapter 10

--- QuizSources/ChimneyAndVentingEssentials/chapter10.js
+++ QuizSources/ChimneyAndVentingEssentials/chapter10.js
@@ -4,6 +4,56 @@ const essentialsChapter10QuizData = [
     options: ["True", "False"],
     answer: "True"
   },
+  {
+    question: "As you arrive at the customer's property, why should you observe the roof? (Page 141)",
+    options: ["You should look for anything unusual, such as leaning or short chimneys", "Because you need to get on the roof for all levels of inspections", "To see what color it is", "Because technicians only service flat roofs"],
+    answer: "You should look for anything unusual, such as leaning or short chimneys"
+  },
+  {
+    question: "If there is something in or around your work area that is already damaged or broken when you get there, you should _________. (Page 143)",
+    options: ["Document it via pictures and bring it to the homeowners attention", "Ignore it because the homeowner likely already knows it is there", "Attempt to fix it", "Only verbally inform the homeowner"],
+    answer: "Document it via pictures and bring it to the homeowners attention"
+  },
+  {
+    question: "Your primary goal is to make the homeowner your friend, not sweep and inspect the chimney with no dust or dirt escaping into the home. (Page 143)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "Downdrafts are more prominent in the Winter months. (Page 143)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "Why would you put a small fan at the top of the flue. (Page 143)",
+    options: ["Assist in pulling dust out of the flue", "To cool off the flue", "To cool yourself off if working on the crown in the summer months", "To dry out creosote"],
+    answer: "Assist in pulling dust out of the flue"
+  },
+  {
+    question: "You should cover the hearth and floor of your work area with _________. (Page 144)",
+    options: ["Your shirt", "A nearby rug", "Clean drop cloths", "Hearth cleaner"],
+    answer: "Clean drop cloths"
+  },
+  {
+    question: "If there are fragile items or family heirlooms in your work area, you should. (Page 144)",
+    options: ["Ask the homeowner to move them", "Move them yourself", "Throw drop cloths over them", "Just try not to hit them"],
+    answer: "Ask the homeowner to move them"
+  },
+  {
+    question: "You should have your vacuum properly positioned and running as soon as possible to lower the risk of soot or dust leakage. (Page 144)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "If there are other chimney openings in the home, apart from the one your are cleaning, you should _________. (Page 144)",
+    options: ["Seal them to prevent dust from entering the home", "Put fans on them", "Work slow", "Leave them open"],
+    answer: "Seal them to prevent dust from entering the home"
+  },
+  {
+    question: "Before going on the roof, you should _________. (Page 145)",
+    options: ["Put your helmet on", "Stretch", "Have the homeowner pay you", "Perform a Level III inspection"],
+    answer: "Put your helmet on"
+  },
 ];
 
 if (!window.quizRegistry) window.quizRegistry = {};
