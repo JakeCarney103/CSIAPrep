a4071917ba61ab2ae13fbe9e325950dae66455e4
Jake Carney <jakecarney103@gmail.com>
2026-01-20 14:48:40 -0500

    Start at page 150 next

--- QuizSources/ChimneyAndVentingEssentials/chapter11.js
+++ QuizSources/ChimneyAndVentingEssentials/chapter11.js
@@ -24,6 +24,71 @@ const essentialsChapter11QuizData = [
     options: ["1/2 the width and 1/2 the depth", "2/3 the width and 1/2-2/3 the depth", "1/2-2/3 the width and 1/2 the depth", "1/4-1/2 the width and 1/4-1/2 the depth"],
     answer: "2/3 the width and 1/2-2/3 the depth"
   },
+  {
+    question: "When it comes to the ash dump door, it should be able to close completely. (Page 147)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "Should there be gaps in between the facade and the sidewalls of the firebox? (Page 147)",
+    options: ["Yes", "No"],
+    answer: "No"
+  },
+  {
+    question: "If the fireplace opening has multiple lintels, what should you do? (Page 147)",
+    options: ["Note it in documentation", "Ignore them", "Only measure the biggest one", "Cut the smaller one out"],
+    answer: "Note it in documentation"
+  },
+  {
+    question: "You are examining the fireplace facade and see smoke staining above the fireplace. What does this mean? (Page 147)",
+    options: ["Most likely they had a fire on the hearth extension", "It can signify a potential performance issue and is an indication of a marginal draft situation", "This is normal and the fireplace is working as expected", "They are burning hardwood"],
+    answer: "It can signify a potential performance issue and is an indication of a marginal draft situation"
+  },
+  {
+    question: "When looking at the damper frame, what could cause excessive air leakage? (Page 147)",
+    options: ["Warped valve plates or frames with mortar in the damper track", "A Majestic Damper", "A Top Damper", "The damper frame is made of cast iron"],
+    answer: "Warped valve plates or frames with mortat in the damper track"
+  },
+  {
+    question: "A _________ is a fixed or removable ring, tube, or lining located in the hole where a chimney connector or vent connector passes into a chimney, smoke chamber or flue. (Page 148)",
+    options: ["Thimble", "Flue Collar", "Slip Connector", "Snout"],
+    answer: "Thimble"
+  },
+  {
+    question: "A metal pie plate thimble cover is safe to use. (Page 148)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "What is the proper way to repair an open thimble or a thimble covered with a decorative cover? (Page 149)",
+    options: ["Close the opening with brick and mortar or an insulated thimble plug", "Remove the decorative cover, sit gravel in the thimble and then replace the decorative cover", "Fill the hole with grout", "Shove a tightly rolled drop cloth or towel in the opening"],
+    answer: "Close the opening with brick and mortar or an isulated thimble plug"
+  },
+  {
+    question: "Pie plate covers have the possibility to leak dust and creosote, so you should cover them or plug them to prevent this. (Page 149)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "_________ are openings usually located near the bottom of the flue or chimney, allowing removal of ashes, dust, creosote and debris. (Page 149)",
+    options: ["Cleanouts", "Throats", "Ash Dumps", "Damper Frames"],
+    answer: "Cleanouts"
+  },
+  {
+    question: "Cleanout doors are usually tight enough to prevent dust from leaking out, so you don't have to seal them up. (Page 149)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "Multiple Flue chimneys can occasionally pose a dust migration problem during cleaning. Dust can leak into the house through unprotected adjacent flue openings when one flue is swept. (Page 149)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "As long as you slowly open the damper, you don't have to have your vacuum runing. (Page 150)",
+    options: ["True", "False"],
+    answer: "True"
+  },
 ];
 
 if (!window.quizRegistry) window.quizRegistry = {};
