c758fb43ce68d746b5e770415202ebeb96ecf7e7
Jake Carney <jakecarney103@gmail.com>
2026-01-19 19:50:15 -0500

    Finish chapter 9

--- QuizSources/ChimneyAndVentingEssentials/chapter9.js
+++ QuizSources/ChimneyAndVentingEssentials/chapter9.js
@@ -24,6 +24,286 @@ const essentialsChapter9QuizData = [
     options: ["Qualified Agency", "Authority Having Jurisdiction", "Chimney Company", "Business Owner"],
     answer: "Qualified Agency"
   },
+  {
+    question: "_________ areas are those that can be reached for inspection or maintenance without the use of tools or ladders. (Page 130)",
+    options: ["Readily Accessible", "Accessible Areas", "Non-Accessible"],
+    answer: "Readily Accessible"
+  },
+  {
+    question: "_________ areas are those that can be reached without destructive action to the building or building finish. Access may require the removal or opening of doors and panels requiring the use of common hand dools or ladders. (Page 130)",
+    options: ["Readily Accessible", "Accessible", "Non-Accessible"],
+    answer: "Accessible Areas"
+  },
+  {
+    question: "_________ areas are those that cannot be reached without damage or destruction to the chimney, building or building finish, or areas that would require special tools to reach. (Page 130)",
+    options: ["Readily Accessible", "Accessible", "Non-Accessible"],
+    answer: "Non-Accessible"
+  },
+  {
+    question: "NFPA 211 recommends that all chimneys, fireplaces and vents be inspected annually. (Page 130)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "NFPA 211 states an inspection should be performed after an operating malfunction, any unusual, or sudden occurrence event, such as a chimney fire, lightning strike, or earthquake. (Page 130)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "NFPA 211 states an inspection should occur prior to purchasing a home with an existing chimney or vent. (Page 130)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "NFPA 211 states an inspection should whenever changes are made to a chimney or vent system, including replacement of connected appliances or installations of other appliances. (Page 130)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "NFPA 211 states an inspection should occur prior to relining or replacement of the system. (Page 130)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "NFPA 211 states an inspection should occur 2 times a year. (Page 130)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "For all inspections, you should verify that the flue is not blocked or significantly restricted or obstructed. (Page 130)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "Checking the general condition of an appliance is part of a _________ inspection at its lowest level. (Page 131)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level I"
+  },
+  {
+    question: "Checking the Appliance Clearance is part of a _________ inspection at its lowest level. (Page 131)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level I"
+  },
+  {
+    question: "Checking the clearance for the appliance and connectors is part of a _________ inspection at its lowest level. (Page 131)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level I"
+  },
+  {
+    question: "Checking the connector joint security (each joint has 3 screws) is part of a _________ inspection at its lowest level. (Page 131)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level I"
+  },
+  {
+    question: "Checking if the hearth extension size is adequate is part of a _________ inspection at its lowest level. (Page 132)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level I"
+  },
+  {
+    question: "Checking the damper moves correctly is part of a _________ inspection at its lowest level. (Page 132)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level I"
+  },
+  {
+    question: "Checking the clearance to combustible trim and mantels around a fireplace opening is part of a _________ inspection at its lowest level. (Page 132)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level I"
+  },
+  {
+    question: "Checking the flue is clear of obstructions is part of a _________ inspection at its lowest level. (Page 132)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level I"
+  },
+  {
+    question: "The use of ladders is required as a part of a Level 1 inspection. (Page 132)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "If performing a Level 1 inspection due to an annual sweeping and you are sweeping from the top, you shouldn't have to inspect things like the crown, brick condition, cap, etc. (Page 132)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "When it comes to appliances and flues, NFPA 211 requires you to inspect _________ when performing strictly a Level I inspection. (Page 133)",
+    options: ["All flues within the chimney and the appliances connected to them", "Only one flue", "Only all flues if there are 3 or more in the same chimney", "All flues, but only one appliance"],
+    answer: "All flues within the chimney and the appliances connected to them"
+  },
+  {
+    question: "When it comes to appliances and flues, NFPA 211 requires you to inspect _________ if you are performing a Level I inspection as a part of the routine sweeping. (Page 133)",
+    options: ["All flues within the chimney and the appliances connected to them", "Only the flue and appliance you are sweeping", "Only all flues if there are 3 or more in the same chimney", "All flues, but only one appliance"],
+    answer: "Only the flue and appliance you are sweeping"
+  },
+  {
+    question: "When it comes to appliances and flues, NFPA 211 requires you to inspect _________ when performing strictly a Level I inspection. (Page 133)",
+    options: ["All flues within the chimney and the appliances connected to them", "Only one flue", "Only all flues if there are 3 or more in the same chimney", "All flues, but only one appliance"],
+    answer: "All flues within the chimney and the appliances connected to them"
+  },
+  {
+    question: "Inspection from the roof, basement and crawl spaces if they are accessible are part of a _________ inspection at its lowest level. (Page 134)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level II"
+  },
+  {
+    question: "Inspection from the roof, basement and crawl spaces if they are accessible are part of a Level I inspection at its lowest level. (Page 134)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "Replacement of an appliance with one of dissimilar type, input rating or efficiency triggers a _________ inspection at its lowest level. (Page 134)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level II"
+  },
+  {
+    question: "Relining or replacement of flue lining triggers a _________ inspection at its lowest level. (Page 134)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level II"
+  },
+  {
+    question: "Sale or transfer of a property triggers a _________ inspection at its lowest level. (Page 134)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level II"
+  },
+  {
+    question: "An event likely to have caused damage to the chimney, such as a fire, operating malfunction or other sudden occurrence event (hurricane, lightning, etc) triggers a _________ inspection at its lowest level. (Page 134)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level II"
+  },
+  {
+    question: "If you recommend a higher level inspection and the client declines you should _________. (Page 134)",
+    options: ["Document their refusal", "Do nothing and move on", "Do it anyway", "Null and void the current inspection you are doing"],
+    answer: "Document their refusal"
+  },
+  {
+    question: "Checking the condition of the chimney is part of a _________ inspection at its lowest level. (Page 134)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level II"
+  },
+  {
+    question: "Checking the attic insulation shield for a factory-built chimney and factory-built fireplace chimney is part of a _________ inspection at its lowest level. (Page 134)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level II"
+  },
+  {
+    question: "Checking the venting system is properly sized for the appliance is part of a _________ inspection at its lowest level. (Page 134)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level II"
+  },
+  {
+    question: "Checking the flashing is part of a _________ inspection at its lowest level. (Page 134)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level II"
+  },
+  {
+    question: "Checking the condition of crickets is part of a _________ inspection at its lowest level. (Page 134)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level II"
+  },
+  {
+    question: "Checking the framing/forms under hearth or hearth extensions of masonry fireplaces, given they are accessible, is part of a _________ inspection at its lowest level. (Page 134)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level II"
+  },
+  {
+    question: "Checking the smoke chamber lining is part of a _________ inspection at its lowest level. (Page 134)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level II"
+  },
+  {
+    question: "Checking the expansion joint between the flue and wash is part of a _________ inspection at its lowest level. (Page 134)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level II"
+  },
+  {
+    question: "Checking the smoke shelf area is part of a _________ inspection at its lowest level. (Page 134)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level II"
+  },
+  {
+    question: "A mirror, flash light and tape measure are considered tools. (Page 134)",
+    options: ["True", "false"],
+    answer: "false"
+  },
+  {
+    question: "If necessary, a Level II inspection requires removal of permanent parts of the building, such as siding, chase covers or wall coverings. (Page 136)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "You are performing a Level II inspection. What should you do when conditions prevent you from completing required elements of the inspection. (Page 136)",
+    options: ["Document it and include why you couldn't perform that part of the inspection", "Completely drop all aspects of the Level II inspection and only perform a Level I", "Do not tell the homeowner about it", "Throw safety aside and attempt to do it anyway"],
+    answer: "Document it and include why you couldn't perform that part of the inspection"
+  },
+  {
+    question: "The most detailed inspection you can do is a _________. (Page 136)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level III"
+  },
+  {
+    question: "Level I inspections require you to inspect areas that are. (Page 131)",
+    options: ["Readily Accessible", "Accessible Areas", "Non-Accessible", "All of these"],
+    answer: "Readily Accessible"
+  },
+  {
+    question: "Level II inspections require you to inspect areas that are. (Page 134)",
+    options: ["Readily Accessible Only", "Accessible Areas Only", "Non-Accessible Only", "Accessible and Readily Accessible Areas", "All of these"],
+    answer: "Accessible and Readily Accessible Areas"
+  },
+  {
+    question: "Level III inspections require you to inspect areas that are. (Page 136)",
+    options: ["Readily Accessible", "Accessible Areas", "Non-Accessible", "All of these"],
+    answer: "All of these"
+  },
+  {
+    question: "If a Level III area requires destruction, you don't have to seek permission first. (Page 137)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "Checking the Firestopping is part of a _________ inspection at its lowest level. (Page 137)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level III"
+  },
+  {
+    question: "Checking the REPLACE is part of a _________ inspection at its lowest level. (Page 137)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level III"
+  },
+  {
+    question: "Checking the Masonry foundation is part of a _________ inspection at its lowest level. (Page 137)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level III"
+  },
+  {
+    question: "Checking the Clearances inside the chimney chase is part of a _________ inspection at its lowest level. (Page 137)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level III"
+  },
+  {
+    question: "Checking the Thickness of fireplace walls is part of a _________ inspection at its lowest level. (Page 137)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level III"
+  },
+  {
+    question: "Checking the smoke chamber wall thickness is part of a _________ inspection at its lowest level. (Page 137)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level III"
+  },
+  {
+    question: "Checking the Factory-Built fireplace hearth strip is properly installed is part of a _________ inspection at its lowest level. (Page 137)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level III"
+  },
+  {
+    question: "NFPA 211 states that flues should be inspected with a camera. (Page 137)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "Checking the Flue and Smoke Chamber with a camera is part of a _________ inspection at its lowest level. (Page 137)",
+    options: ["Level I", "Level II", "Level III"],
+    answer: "Level II"
+  },
 ];
 
 if (!window.quizRegistry) window.quizRegistry = {};
