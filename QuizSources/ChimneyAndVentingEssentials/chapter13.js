9de189afec134903f60771c43d5f9cc8fa405d4f
Jake Carney <jakecarney103@gmail.com>
2026-01-22 15:11:26 -0500

    Start at page 180 next

--- QuizSources/ChimneyAndVentingEssentials/chapter13.js
+++ QuizSources/ChimneyAndVentingEssentials/chapter13.js
@@ -1,13 +1,73 @@
 const essentialsChapter13QuizData = [
   {
-    question: "One of the last things you should do before sweeping is roll out drop cloths, runners or other covering from the entry point of the home to your work area. (Page 147)",
+    question: "You get to a house to sweep and inspect a Freestanding Stove. Installation instructions are not available, neither is a data plate. What is the clearance for this stove? (Page 177)",
+    options: ["36 inches", "18 inches", "9 inches", "12 inches"],
+    answer: "36 inches"
+  },
+  {
+    question: "You are sweeping and inspecting a Freestanding Stove. It has single-wall connector pipe. What is the clearance for that pipe? (Page 177)",
+    options: ["36 inches", "9 inches", "18 inches", "6 inches"],
+    answer: "18 inches"
+  },
+  {
+    question: "Double-wall stovepipes may have as low as a _________ clearance requirement to combustibles. (Page 177)",
+    options: ["36 inches", "9 inches", "18 inches", "6 inches"],
+    answer: "6 inches"
+  },
+  {
+    question: "You are sweeping and inspecting a Freestanding Stove. You need to make sure the rise of the connector pipe is correct. What should it be? (Page 177)",
+    options: ["1/4 inch per foot", "1/4 foot per inch", "1/4 inch per inch", "1 foot per 3 feet"],
+    answer: "1/4 inch per foot"
+  },
+  {
+    question: "The customer asks you to buy a brand of factory-built chimney pipe that differs from the manufacturers instructions. Is it alright to mix brands or types? (Page 177)",
+    options: ["Yes", "No"],
+    answer: "No"
+  },
+  {
+    question: "Appliances should not be installed in a confined space less than _________ unless listed for use in such places and installed in accordance with the manufacturer's instructions. (Page 177)",
+    options: ["512 cubic feet", "671 cubic feet", "512 square feet", "1000 square inches"],
+    answer: "512 cubic feet"
+  },
+  {
+    question: "The cross-sectional area of the flue should be no greater than _________ the cross-sectional area of the appliance flue collar. (Page 178)",
+    options: ["3 times", "4 times", "1.5 times", "3.5 times"],
+    answer: "3 times"
+  },
+  {
+    question: "The connector pipe and chimney diameter should _________ than the appliance flue collar unless listed for use by the appliance manufacturer. (Page 178)",
+    options: ["Never be smaller", "Always be 2 times larger", "Always be 1 inch smaller", "Never be the same size"],
+    answer: "Never be smaller"
+  },
+  {
+    question: "Any floor protection or hearth extension should extend at least _________ beyond the appliance on all sides, unless listed by the appliance manufacturer. (Page 178)",
+    options: ["36 inches", "18 inches", "6 inches", "12 inches"],
+    answer: "18 inches"
+  },
+  {
+    question: "Stove installations vented through a wall to a factory-built metal chimney require _________. (Page 178)",
+    options: ["A Listed Wall Pass-Through", "Single-Wall Stove Pipe", "Roof Support", "A Power Vent"],
+    answer: "A Listed Wall Pass-Through"
+  },
+  {
+    question: "When a connector pipe extends to a ceiling-supported or roof supported factory-built chimney, often a specific adaptor doesn't have to be installed at or below the ceiling. (Page 178)",
     options: ["True", "False"],
     answer: "False"
   },
   {
-    question: "To guard against exposure to creosote and soot, what should you do? (Page 147)",
-    options: ["Wear a helmet", "Wear all related PPE", "Turn on a fan", "Hold your breath"],
-    answer: "Wear all related PPE"
+    question: "A factory-built chimney enclosed in a chase often cools faster than a factory-built chimney that is just bare pipe sticking out of the roofline. (Page 179)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "If a freestanding stove vents straight up, you still have to disconnect the connector pipe. (Page 178)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "If other appliances operate on the same flue as the stove, can that violate wood burning appliance installation instructions and/or safety listing? (Page 178)",
+    options: ["Yes", "No"],
+    answer: "Yes"
   },
 ];
 
