1df02379257a66de0170fb54168d8f980809d5bd
Jake Carney <jakecarney103@gmail.com>
2026-01-22 14:22:25 -0500

    Finish Chapter 12

--- QuizSources/ChimneyAndVentingEssentials/chapter12.js
+++ QuizSources/ChimneyAndVentingEssentials/chapter12.js
@@ -24,6 +24,231 @@ const essentialsChapter12QuizData = [
     options: ["True", "False"],
     answer: "False"
   },
+  {
+    question: "With a Factory-Built Fireplace, rust indicates _________. (Page 166)",
+    options: ["Water Entry or a High Heat Incident", "High Heat Incident or Low Heat Incident", "Water Entry Only", "Water Entry or Burning Pine"],
+    answer: "Water Entry or a High Heat Incident"
+  },
+  {
+    question: "Most factory-built chimneys require _________ to combustibles. (Page 166)",
+    options: ["2 inches", "36 inches", "18 inches", "6 inches"],
+    answer: "2 inches"
+  },
+  {
+    question: "With a Factory-Built Fireplace, UL requires that each section of the chimney have an arrow pointing towards the appliance. (Page 166)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "Air circulation inlets and outlets that are blocked trap heat inside the appliance which is a hazard. (Page 166)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "How should you open the damper? (Page 166)",
+    options: ["Fast", "Slowly", "Using a sweeping rod", "Factory-Built Fireplaces don't have a damper to open"],
+    answer: "Slowly"
+  },
+  {
+    question: "Once the damper is open and you look up into it, what will signal that a Factory-Built Chimney Flue should be swept. (Page 166)",
+    options: ["Any buildup what-so-ever", "Only if you see hard and glossy deposits", "If you can see outside", "If you see buildup in the smoke chamber"],
+    answer: "Any buildup what-so-ever"
+  },
+  {
+    question: "On many factory-uilt units, the log grate is not secured to the back wall. It just freely hangs out in the middle of the appliance. (Page 167)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "If sweeping from the top, how should you have the damper most of the time? (Page 167)",
+    options: ["Closed", "Open", "Removed", "Cracked"],
+    answer: "Closed"
+  },
+  {
+    question: "With a Factory-Built Fireplace, where should you have the vacuum placed? (Page 168)",
+    options: ["At the rear of the firebox", "At the front of the firebox", "On the smoke shelf", "At the top of the flue"],
+    answer: "At the rear of the firebox"
+  },
+  {
+    question: "You are getting ready to sweep a chimney and see that is has a downdraft. You can hear an exhaust fan. What should you do? (Page 168)",
+    options: ["Ask the owner to shut off any exhaust fans, air handling devices, or ceiling fans", "Nothing because most likely it is in the kitchen or bathroom", "Turn the ceiling fan on in the room that you are working in to combat it", "Nothing and proceed to sweep the chimney"],
+    answer: "Ask the owner to shut off any exhaust fans, air handling devices, or ceiling fans"
+  },
+  {
+    question: "To slow air exchanges and isolate your work area from the rest of the home, you can simply shut the doors and windows to the room. (Page 168)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "Downdrafts are accelerated in homes that have a positive pressure issue. (Page 168)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "Downdrafts are commonly encountered in _________? (Page 168)",
+    options: ["Warm Weather", "Snowy Weather", "Freezing Weather", "Rainy Weather"],
+    answer: "Warm Weather"
+  },
+  {
+    question: "Downdrafts are commonly encountered in chimneys located _________? (Page 168)",
+    options: ["Outside the building envelope", "Inside the building envelope", "In Ohio", "The main floor"],
+    answer: "Outside the building envelope"
+  },
+  {
+    question: "If you remove a cap to sweep from above, whose responsibility is it to reattach it. (Page 169)",
+    options: ["Yours", "The Homeowner's", "Nobody's", "Your assistant"],
+    answer: "Yours"
+  },
+  {
+    question: "Your preliminary exam of the fireplace and chimney will not provide you with enough information to determine if it is best to sweep from above or below. (Page 168)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "If the chimney is too tall or inaccessible, you should sweep from _________. (Page 168)",
+    options: ["Above", "Below"],
+    answer: "Below"
+  },
+  {
+    question: "If the chimney is in poor condition, you should sweep from _________. (Page 168)",
+    options: ["Above", "Below"],
+    answer: "Below"
+  },
+  {
+    question: "If there is a snow storm outside, you should sweep from _________. (Page 168)",
+    options: ["Above", "Below"],
+    answer: "Below"
+  },
+  {
+    question: "If the chimney is easily accessible, you should sweep from _________. (Page 168)",
+    options: ["Above", "Below"],
+    answer: "Above"
+  },
+  {
+    question: "If the chimney in in good condition and the weather is fine outside, you should sweep from _________. (Page 168)",
+    options: ["Above", "Below"],
+    answer: "Above"
+  },
+  {
+    question: "If the chimney has a major downdraft, you should sweep from _________. (Page 168)",
+    options: ["Above", "Below"],
+    answer: "Below"
+  },
+  {
+    question: "If you notice severely cracked flue tiles, you should sweep from _________. (Page 168)",
+    options: ["Above", "Below"],
+    answer: "Below"
+  },
+  {
+    question: "Caps manufactured for Factory-Built Chimney Systems are typically secured with _________. (Page 169)",
+    options: ["All of these", "A Clamping System", "Screws", "Twist Lock", "Friction Fit"],
+    answer: "All of these"
+  },
+  {
+    question: "It is not very common for soot, organic vapors and creosote to come out of the top of the flue when sweeping _________. (Page 169)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "When it comes to the rod system chosen to sweep a Factory-Built Chimney System, your rods should have _________. (Page 169)",
+    options: ["Connectors of the same diameter as the rods", "Metal Brushes Attached", "Grease on them", "Connectors of a larger diameter than the rods"],
+    answer: "Connectors of the same diameter as the rods"
+  },
+  {
+    question: "Metal Brushes should be used on a Factory-Built System. (Page 169)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "If you are sweeping from the roof or below, does that dictate the parts of the system you clean? On top of that, what parts should you clean? (Page 169)",
+    options: ["No it doesn't dictate the parts you clean and you should clean all parts.", "Yes it does dictate the parts you clean and you should only clean the flue.", "No it doesn't dictate the parts you clean and you should only clean the flue", "No it doesn't dictate the parts you clean and you should always clean only the flue and cap."],
+    answer: "No it doesn't dictate the parts you clean and you should clean all parts."
+  },
+  {
+    question: "Homeowners usually know when a chimney fire has occurred. (Page 170)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "When it comes to a Factory-Built Chimney System, warped or damaged joints and pipe could signify a chimney fire. (Page 170)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "Evidence of a chimney fire event requires a _________ inspection as recommended in NFPA 211. (Page 170)",
+    options: ["Level I", "Level II", "Level III", "No Inspection is Required"],
+    answer: "Level II"
+  },
+  {
+    question: "If there is a plug in the chimney, usually what side is softer? (Page 171)",
+    options: ["Top", "Bottom", "Inside", "The side closer to an offset"],
+    answer: "Bottom"
+  },
+  {
+    question: "If you discover a solid plug in the flue, it is best to pound on it with a weight from the top? (Page 171)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "As a last resort, if all other techniques have failed to clear a plugged flue in a Factory-Built Chimney System, what can you do? (Page 172)",
+    options: ["Call the fire department", "Just note to the homeowner not to use it anymore", "Replace the entire venting system", "Use a small amount of tannerite"],
+    answer: "REplace the entire venting system"
+  },
+  {
+    question: "_________ are required to properly sweep offsets. (Page 172)",
+    options: ["Flexible Rods", "Chemical Cleaners", "Long Arms", "Flat Wire Brushes"],
+    answer: "Flexible Rods"
+  },
+  {
+    question: "Typically, additional support is required when a Factory-Built Chimeny protrudes _________ above the roof. (Page 173)",
+    options: ["4 or 5 feet", "1 foot", "3 or 4 feet", "2 foot"],
+    answer: "4 or 5 feet"
+  },
+  {
+    question: "Many factory-built chimneys are enclosed in a(n) _________. (Page 173)",
+    options: ["Chase", "Run", "Cap", "Offset"],
+    answer: "Chase"
+  },
+  {
+    question: "Deformations of the outer chimney wall, such as corroded or pitted chimney parts, dented chimney walls, and holes or buckling in chimney liners, can cause _________. (Page 173)",
+    options: ["Better Draft", "Hot Spots", "Cold Spots", "Backpuffing"],
+    answer: "Hot Spots"
+  },
+  {
+    question: "A(n) _________ ensures water runs off the chase top and not down the inside of the chase during rainstorms. (Page 173)",
+    options: ["Chase Cover", "Crown", "Wash", "Splay"],
+    answer: "Chase Cover"
+  },
+  {
+    question: "As a part of the final examination and Level I Inspection, you must make sure the flue is not blocked. (Page 174)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "You should use _________ brushes to sweep Factory-Built Chimney Systems. (Page 174)",
+    options: ["Polypropylene", "Round Steel", "Flat Wire Brushes", "Potbrushes"],
+    answer: "Polypropylene"
+  },
+  {
+    question: "Every time you sweep a chimney a _________ is required. (Page 175)",
+    options: ["Level I", "Level II", "Level III", "Category II"],
+    answer: "Level I"
+  },
+  {
+    question: "When looking at Refractory Panels in a firebox these indicate the panel should be replaced. (Page 175)",
+    options: ["Large Cracks", "Crumbling Walls", "Either of These"],
+    answer: "Either of These"
+  },
+  {
+    question: "According to the NFPA 211, chimney and venting systems should be inspected once a year and swept when necessary. (Page 176)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "After you share the report with the homeowner, if possible have them _________. (Page 176)",
+    options: ["Sign the report", "Shake your hand", "Only verbally acknowledge the report", "Prove they know how to use their fireplace"],
+    answer: "Sign the report"
+  },
 ];
 
 if (!window.quizRegistry) window.quizRegistry = {};
