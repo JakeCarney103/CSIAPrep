4f1ccfc361583ee4884afa7973601de381b085a7
Jake Carney <jakecarney103@gmail.com>
2026-01-22 14:45:06 -0500

    Create chapter13.js

---
+++ QuizSources/ChimneyAndVentingEssentials/chapter13.js
@@ -0,0 +1,15 @@
+const essentialsChapter13QuizData = [
+  {
+    question: "One of the last things you should do before sweeping is roll out drop cloths, runners or other covering from the entry point of the home to your work area. (Page 147)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "To guard against exposure to creosote and soot, what should you do? (Page 147)",
+    options: ["Wear a helmet", "Wear all related PPE", "Turn on a fan", "Hold your breath"],
+    answer: "Wear all related PPE"
+  },
+];
+
+if (!window.quizRegistry) window.quizRegistry = {};
+window.quizRegistry['Chapter 13: Sweeping Freestanding Stove Chimneys'] = essentialsChapter13QuizData;
