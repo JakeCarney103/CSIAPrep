56945030c02d986e09147b9013604d04a9445804
Jake Carney <jakecarney103@gmail.com>
2026-01-26 13:05:28 -0500

    Add chapter 18, start at 227

---
+++ QuizSources/ChimneyAndVentingEssentials/chapter18.js
@@ -0,0 +1,56 @@
+const essentialsChapter18QuizData = [
+  {
+    question: "Nitrogen must be drawn into the appliance to sustain combustion. (Page 223)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "Chimney Draft is measured in _________. (Page 224)",
+    options: ["Inches of Water Column", "Miles Per Hour", "Pounds Per Square Inch", "Feet Per Second"],
+    answer: "Inches of Water Column"
+  },
+  {
+    question: "Which of the following is the greatest draft? (Page 224)",
+    options: ["-.12 inches w.c.", "-.08 inches w.c.", "-.04 inches w.c.", ".01 inches w.c."],
+    answer: "-.12 inches w.c."
+  },
+  {
+    question: "The draft in the venting system must be high enough to overcome and POSITIVE air pressure in the home. (Page 225)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "_________ to _________ is withing an acceptable range for a draft reading. (Page 225)",
+    options: ["-.06 inches w.c. to -.15 inches w.c.", "-.08 inches w.c. to -.17 inches w.c.", "0 inches w.c. to -.04 w.c.", "-.15 inches w.c to .15 inches w.c."],
+    answer: "-.06 inches w.c. to -.15 inches w.c."
+  },
+  {
+    question: "If draft immediately increases when opening a window or a door in the house, you can safely assume you have a _________. (Page 225)",
+    options: ["Air Pressure Problem.", "Wood Moisture Problem", "Appliance Problem", "Smoke Shelf Problem"],
+    answer: "Air Pressure Problem"
+  },
+  {
+    question: "Ideal wood moisture content is between _________ and _________. (Page 225)",
+    options: ["15% and 25%", "10% and 25%", "20% and 25%", "15% and 30%"],
+    answer: "15% and 15%"
+  },
+  {
+    question: "You are inspecting a homeowner's wood pile. You see that they have the top and sides covered in a tarp. Is this an issue? (Page 226)",
+    options: ["Yes because covering the sides 'seals' the moisture inside the wood.", "Not an issue at all. This is proper storage.", "Yes because the tarp traps the heat inside and can make the wood too dry.", "Yes because the wood should be stored uncovered."],
+    answer: "Yes because covering the sides 'seals' the moisture inside the wood."
+  },
+  {
+    question: "Fireplaces may have an overall efficiency of less than 10% and in some cases may even take more heat out of the room than they put in. (Page 226)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "Smoke and Flue Gases being forced down the chimney because of high pressure at the top of the flue indicate _________. (Page 226)",
+    options: ["Wind-Induced Downdrafts", "Stack Effect", "Proper function", "Bad burning habits"],
+    answer: "Wind-Induced Downdrafts"
+  },
+];
+
+
+if (!window.quizRegistry) window.quizRegistry = {};
+window.quizRegistry['Chapter 18: Troubleshooting'] = essentialsChapter18QuizData;
