179de7352754d80ff880128a102eccb31d2e99b6
Jake Carney <jakecarney103@gmail.com>
2026-01-27 13:02:47 -0500

    Add chapter 19

--- QuizSources/ChimneyAndVentingEssentials/chapter19.js
+++ QuizSources/ChimneyAndVentingEssentials/chapter19.js
@@ -4,6 +4,46 @@ const essentialsChapter19QuizData = [
     options: ["True", "False"],
     answer: "True"
   },
+  {
+    question: "If you perform some part of a higher level of inspection, you are required to fully complete that higher level. (Page 245)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "After presenting your documentation to the homeowner, you should try to _________. (Page 245)",
+    options: ["Get them to sign an acknowledgment", "Only have them verbally agree they understand the issues", "Call the office", "Sell them a full chimney rebuild"],
+    answer: "Get them to sign an acknowledgment"
+  },
+  {
+    question: "If the homeowner refuses a part of the inspection, you should _________. (Page 245)",
+    options: ["Exclude that from your report entirely", "Document the refusal reason and the area that was refused", "Perform that part anyway", "Automatically drop the inspection level down by 1"],
+    answer: "Document the refusal reason and the area that was refused"
+  },
+  {
+    question: "If a connected appliance is removed, and the chimney use is discontinued, an inspection is not required. (Page 247)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "Proper documentation is important for many reasons. One of those reasons is because it could be presented if a claim or lawsuit occurs. (Page 247)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "If a real estate transfer occurs, what level of inspection is required? (Page 247)",
+    options: ["Level I", "Level II", "Level III", "None"],
+    answer: "Level II"
+  },
+  {
+    question: "When documenting, the information doesn't have to flow logically. (Page 247)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "These words should never be a part of a technician's vocabulary when referring to chimneys and vents. (Page 248)",
+    options: ["Safe and Unsafe", "Sweep and Chemicals", "Proposal and Fix", "Code and Regulation"],
+    answer: "Safe and Unsafe"
+  },
 ];
 
 
