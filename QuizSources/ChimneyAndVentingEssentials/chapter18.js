e28d5a529ddc7967cd173ca39d1cf00e3a1a65eb
Jake Carney <jakecarney103@gmail.com>
2026-01-26 20:44:31 -0500

    page 228 next

--- QuizSources/ChimneyAndVentingEssentials/chapter18.js
+++ QuizSources/ChimneyAndVentingEssentials/chapter18.js
@@ -26,13 +26,13 @@ const essentialsChapter18QuizData = [
   },
   {
     question: "If draft immediately increases when opening a window or a door in the house, you can safely assume you have a _________. (Page 225)",
-    options: ["Air Pressure Problem.", "Wood Moisture Problem", "Appliance Problem", "Smoke Shelf Problem"],
+    options: ["Air Pressure Problem", "Wood Moisture Problem", "Appliance Problem", "Smoke Shelf Problem"],
     answer: "Air Pressure Problem"
   },
   {
     question: "Ideal wood moisture content is between _________ and _________. (Page 225)",
     options: ["15% and 25%", "10% and 25%", "20% and 25%", "15% and 30%"],
-    answer: "15% and 15%"
+    answer: "15% and 25%"
   },
   {
     question: "You are inspecting a homeowner's wood pile. You see that they have the top and sides covered in a tarp. Is this an issue? (Page 226)",
@@ -48,6 +48,91 @@ const essentialsChapter18QuizData = [
     question: "Smoke and Flue Gases being forced down the chimney because of high pressure at the top of the flue indicate _________. (Page 226)",
     options: ["Wind-Induced Downdrafts", "Stack Effect", "Proper function", "Bad burning habits"],
     answer: "Wind-Induced Downdrafts"
+  },
+  {
+    question: "Wind-induced downdrafts are commonly caused by the airflow relationship between the house and the chimney termination. (Page 226)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "Wind blowing against a barrier can create a _________ zone on the windward side as the air piles up waiting to get around the barrier. (Page 227)",
+    options: ["High-Pressure", "Low-Pressure", "Neutral-Pressure"],
+    answer: "High-Pressure Zone"
+  },
+  {
+    question: "When a chimney top is situated in a high-pressure zone caused by wind blowing against a barrier and piling up, the smoke flows _________. (Page 227)",
+    options: ["Downward because the draft in the chimney is usually overcome", "Upward because the draft in the chimney tends to go to the high-pressure"],
+    answer: "Downward because the draft in the chimney is usually overcome"
+  },
+  {
+    question: "A chimney should extend _________ above the roof line. (Page 227)",
+    options: ["2 feet", "3 feet", "4 feet", "30 inches"],
+    answer: "3 feet"
+  },
+  {
+    question: "A chimney should extend _________ above anything in a 10 foot radius. (Page 227)",
+    options: ["2 feet", "3 feet", "4 feet", "30 inches"],
+    answer: "2 feet"
+  },
+  {
+    question: "A chimney should extend 2 feet above anything in a _________ radius. (Page 227)",
+    options: ["2 feet", "3 feet", "12 feet", "10 feet"],
+    answer: "10 feet"
+  },
+  {
+    question: "To overcome stack effect an option would be to make sure the chimney is taller than the roof peak, along with the 3-2-10 rule. (Page 227)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "Some chimney caps can help fix win-induced downdraft caused by turbulence. (Page 227)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "What wins in a competition for air; mechanical systems or natural draft systems? (Page 227)",
+    options: ["Mechanical Systems", "Natural Draft Systems"],
+    answer: "Mechanical Systems"
+  },
+  {
+    question: "_________ is a condition where the heated air inside the home rises to the highest level of the house and escapes through various leakage area?. (Page 228)",
+    options: ["Pressure", "House Stack Effect", "Flow", "Turbulence"],
+    answer: "House Stack Effect"
+  },
+  {
+    question: "The taller the house is, the _________ the stack effect. (Page 228)",
+    options: ["Greater", "Smaller", "House height makes no difference"],
+    answer: "Greater"
+  },
+  {
+    question: "Appliances in this location are more susceptible to problems caused by stack effect. (Page 228)",
+    options: ["Basement", "Main Floor", "Upstairs"],
+    answer: "Basement"
+  },
+  {
+    question: "Appliances and chimney openings located _________ the Neutral Pressure Plane of the house must work against negative pressure induced by stack effect. (Page 228)",
+    options: ["On", "Above", "Below"],
+    answer: "Below"
+  },
+  {
+    question: "Appliances and chimney openings located _________ the Neutral Pressure Plane of the house are assisted by the positive pressure induced by stack effect. (Page 228)",
+    options: ["On", "Above", "Below"],
+    answer: "Above"
+  },
+  {
+    question: "A house with similar leakage rates at all levels will have a neutral pressure plane around _________. (Page 228)",
+    options: ["The house vertical mid-point", "The basement", "The attic"],
+    answer: "The house vertical mid-point"
+  },
+  {
+    question: "The neutral pressure plane of a house will move towards the leakiest level. (Page 228)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "Back drafting at standby is clear evidence of _________. (Page 228)",
+    options: ["Faulty Appliance", "Stack Effect", "Creosote", "The chimney being above the roof's peak"],
+    answer: "Stack Effect"
   },
 ];
 
