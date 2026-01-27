68b9c8cb90eaa6800151b5e9bb45d1d4ddffe8e9
Jake Carney <jakecarney103@gmail.com>
2026-01-27 12:12:02 -0500

    Add initial chapter 19

---
+++ QuizSources/ChimneyAndVentingEssentials/chapter19.js
@@ -0,0 +1,11 @@
+const essentialsChapter19QuizData = [
+  {
+    question: "You should collect documentation based on the level of inspection you are performing. (Page 245)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+];
+
+
+if (!window.quizRegistry) window.quizRegistry = {};
+window.quizRegistry['Chapter 19: Documentation'] = essentialsChapter19QuizData;
--- masterKey.js
+++ masterKey.js
@@ -45,8 +45,8 @@ const masterKey = [
 	  { quizName: "Chapter 16: Sweeping Gas and Oil Appliance Chimneys or Vents", fileName: "chapter16.js" },
 	  { quizName: "Chapter 17: Sweeping Masonry Heaters", fileName: "chapter17.js" },
 	  { quizName: "Chapter 18: Troubleshooting", fileName: "chapter18.js" },
-	  /*
 	  { quizName: "Chapter 19: Documentation", fileName: "chapter19.js" },
+	  /*
 	  { quizName: "Chapter 20: Advising the Customer and Maintenance", fileName: "chapter20.js" },
 	  */
     ]
