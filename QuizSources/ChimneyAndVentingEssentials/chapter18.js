f9fbb0ad8906c1132d3fa64a6ab584868e6b1029
Jake Carney <jakecarney103@gmail.com>
2026-01-27 12:09:04 -0500

    Finish chapter 18

--- QuizSources/ChimneyAndVentingEssentials/chapter18.js
+++ QuizSources/ChimneyAndVentingEssentials/chapter18.js
@@ -55,8 +55,8 @@ const essentialsChapter18QuizData = [
     answer: "True"
   },
   {
-    question: "Wind blowing against a barrier can create a _________ zone on the windward side as the air piles up waiting to get around the barrier. (Page 227)",
-    options: ["High-Pressure", "Low-Pressure", "Neutral-Pressure"],
+    question: "Wind blowing against a barrier can create a _________ on the windward side as the air piles up waiting to get around the barrier. (Page 227)",
+    options: ["High-Pressure Zone", "Low-Pressure Zone", "Neutral-Pressure Zone"],
     answer: "High-Pressure Zone"
   },
   {
@@ -130,9 +130,294 @@ const essentialsChapter18QuizData = [
     answer: "True"
   },
   {
-    question: "Back drafting at standby is clear evidence of _________. (Page 228)",
+    question: "The further the appliance is ABOVE the neutral pressure plane, the bigger the challenge to draft. (Page 228)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "A chimney installed on the INSIDE of a house serving a basement installation is most susceptible to back drafting or spillage caused by stack effect. (Page 229)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "Back drafting at standby is clear evidence of performance issues such as  _________. (Page 229)",
     options: ["Faulty Appliance", "Stack Effect", "Creosote", "The chimney being above the roof's peak"],
     answer: "Stack Effect"
+  },
+  {
+    question: "To reverse the down draft you can use a burning rolled up piece of cardboard or paper to heat the flue. (Page 229)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "If there are leaks in the house, which location, if sealed up, would reduce stack effect? (Page 229)",
+    options: ["Top Floor", "Basement", "Main Floor"],
+    answer: "Top Floor"
+  },
+  {
+    question: "Raising the height of a chimney to _________ or more above the peak of a house will defeat stack effect in most cases. (Page 230)",
+    options: ["12 inches", "2 inches", "6 inches", "8 inches"],
+    answer: "12 inches"
+  },
+  {
+    question: "There is a chimney with 2 flues. One serves a basement appliance, while the other serves a main floor fireplace. The tops of these flues are close to each other and are the same height. However, some smoke is being drawn through the basement's appliance when these are in use. What could be a possible fix for this? (Page 230)",
+    options: ["Extend the Lowest Level Appliance's flue by about 12 inches", "Extend the Highest Level Appliance's flue by about 12 inches", "Extend both flues by about 12 inches", "Extend the Lowest Level Appliance's flue by 6 inches"],
+    answer: "Extend the Highest Level Appliance's flue by about 12 inches"
+  },
+  {
+    question: "Leaky ductwork has no effect on the home's air pressure. (Page 231)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "A _________ is usually created on the downwind side of a house. (Page 231)",
+    options: ["High-Pressure Zone", "Low-Pressure Zone", "Neutral-Pressure Zone"],
+    answer: "Low-Pressure Zone"
+  },
+  {
+    question: "A Mechanical Draft Inducer should always be installed at the bottom of the chimney, whether they are listed for a residential chimney or not. (Page 231)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "It is best to bring make-up air in on the _________ level of the home to balance the whole house. (Page 231)",
+    options: ["Highest", "Lowest"],
+    answer: "Lowest"
+  },
+  {
+    question: "A chimney that has a mechanical draft inducer will always draft properly during a power outage. (Page 231)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "A furnace takes the most air from the home. It also may take air from the place of least resistance, which is usually the fireplace flue. (Page 232)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "Fireplaces consume between _________ of room air, which could be an issue in airtight homes due to a fireplace's large flow requirements. (Page 232)",
+    options: ["100 to 500 cubic feet per minute", "500 to 1000 cubic feet per minute", "150 to 350 cubic feet per minute", "50 to 200 cubic feet per minute"],
+    answer: "100 to 500 cubic feet per minute"
+  },
+  {
+    question: "The cross-sectional area of a round flue should be _________ the size of the fireplace opening. (Page 233)",
+    options: ["1/12", "1/20", "1/10", "1/8"],
+    answer: "1/12"
+  },
+  {
+    question: "The cross-sectional area of a square or rectangular flue with an aspect ratio of less than 2:1 should be _________ the size of the fireplace opening. (Page 233)",
+    options: ["1/12", "1/20", "1/10", "1/8"],
+    answer: "1/10"
+  },
+  {
+    question: "The cross-sectional area of a square or rectangular flue with an aspect ratio of more than 2:1 should be _________ the size of the fireplace opening. (Page 233)",
+    options: ["1/12", "1/20", "1/10", "1/8"],
+    answer: "1/8"
+  },
+  {
+    question: "A fireplace opening is 36 inches x 24 inches. Out of the options below, what size square or rectangle flue is the best to use? (Page 233)",
+    options: ["8 x 8", "8 x 12", "10 x 10", "12 x 12"],
+    answer: "8 x 12"
+  },
+  {
+    question: "A fireplace opening is 36 inches x 24 inches. Out of the options below, what size round flue is the best to use? (Page 233)",
+    options: ["8 inch", "6 inch", "10 inch", "12 inch"],
+    answer: "10 inch"
+  },
+  {
+    question: "A fireplace opening has an 8 x 17 flue. Out of the options below, what size fireplace opening is the max that the flue could handle? (Page 233)",
+    options: ["36 inches x 24 inches", "48 inches x 24 inches", "40 inches x 26 inches", "38 inches x 24 inches"],
+    answer: "40 inches x 26 inches"
+  },
+  {
+    question: "According to the NFPA, spark screens should have _________ times the net free area of the outlet? (Page 234)",
+    options: ["3", "4", "5", "2"],
+    answer: "3"
+  },
+  {
+    question: "According to the IRC, spark screens should have _________ times the net free area of the outlet? (Page 234)",
+    options: ["3", "4", "5", "2"],
+    answer: "4"
+  },
+  {
+    question: "Spark screens cannot allow the passage of a _________ sphere. (Page 234)",
+    options: ["1/2 inch", "1/4 inch", "1/8 inch", "3/8 inch"],
+    answer: "1/2 inch"
+  },
+  {
+    question: "Spark screens should allow passage of spheres less than _________ in diameter. (Page 234)",
+    options: ["1/2 inch", "1/4 inch", "3/8 inch", "3/4 inch"],
+    answer: "3/8 inch"
+  },
+  {
+    question: "If you take a 5/8 inch diameter sphere and attempt to put it through a spark screen, it should _________. (Page 234)",
+    options: ["Pass through", "Be blocked"],
+    answer: "Be blocked"
+  },
+  {
+    question: "If you take a 1/4 inch diameter sphere and attempt to put it through a spark screen, it should _________. (Page 234)",
+    options: ["Pass through", "Be blocked"],
+    answer: "Pass through"
+  },
+  {
+    question: "According to the NFPA, if the flue is 10 inches x 10 inches, the spark screen should have a net free area of at least _________. (Page 234)",
+    options: ["300 square inches", "100 square inches", "250 square inches", "350 square inches"],
+    answer: "300 square inches"
+  },
+  {
+    question: "Chimney flues can have a maximum offset of _________ from vertical. (Page 234)",
+    options: ["30°", "45°", "60°", "90°"],
+    answer: "30°"
+  },
+  {
+    question: "Ideally the ratio of the height of the firebox opening should be _________ of the firebox opening width. (Page 234)",
+    options: ["3/4", "1/2", "2/3", "5/8"],
+    answer: "2/3"
+  },
+  {
+    question: "NFPA and IRC guidelines are the _________ standards acceptable. (Page 234)",
+    options: ["Minimum", "Maximum"],
+    answer: "Minimum"
+  },
+  {
+    question: "The flue should be _________ in the smoke chamber. (Page 234)",
+    options: ["Offset to the left", "Offset to the right", "Centered", "At the very back"],
+    answer: "Centered"
+  },
+  {
+    question: "Smoke chamber height should be no more than _________. (Page 234)",
+    options: ["The height of the firebox opening", "The width of the firebox opening", "The depth of the firebox inner hearth", "1/4 height of the flue"],
+    answer: "The width of the firebox opening"
+  },
+  {
+    question: "A smoke chamber should be no more than _________ angled from vertical. (Page 234)",
+    options: ["30°", "45°", "60°", "90°"],
+    answer: "45°"
+  },
+  {
+    question: "Smoke chambers don't have to be parged smooth. (Page 234)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "Using a single flue to vent more than one appliance can cause inadequate flow. (Page 235)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "Unless a wood burner is specifically tested and listed for such sue, you shouldn't vent a solid fuel appliance through a flue that serves any other appliance. (Page 235)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "If the catalyst has crumbling substrate/materials on a Catalytic Combustor, the customer should _________. (Page 236)",
+    options: ["Bypass the catalytic combustor when the stove is running in a high-fire mode", "Reduce combustion air to slow the fire down", "Build smaller fires", "Any of these"],
+    answer: "Any of these"
+  },
+  {
+    question: "For Catalytic Combustors, 'Light Off' (The temp at which the catalyst becomes effective) happens at _________. (Page 236)",
+    options: ["500°F", "1000°F", "1200°F", "450°F"],
+    answer: "500°F"
+  },
+  {
+    question: "A general rule of thumb for venting EPA Certified stoves is to provide a minimum of _________ of vertical (connector + chimney) venting. (Page 237)",
+    options: ["5 feet", "10 feet", "15 feet", "20 feet"],
+    answer: "15 feet"
+  },
+  {
+    question: "Connector pipe should be as long as possible. (Page 237)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "Connector pipe should be as long as possible. (Page 237)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "Manufactures may recommend the venting system height be increased _________ per 1000 feet above sea level. (Page 237)",
+    options: ["2% to 3%", "2% to 5%", "1% to 2%", "4% to 6%"],
+    answer: "2% to 3%"
+  },
+  {
+    question: "On a new stove, you should have a few small fires before burning it very hot to cure the paint. (Page 238)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "Which option increases the likelihood that the glass will get dirty on a stove. (Page 239)",
+    options: ["Poor wood quality", "Chimney is 5 feet above the roof line", "The stove's connector pipe is 4 feet long and rises 1 inch in total", "The chimney is located on the inside of the building envelope"],
+    answer: "Poor wood quality"
+  },
+  {
+    question: "A customer is saying they can't get their fire to start. They are opening the primary air control. They are using good wood that is 18% moisture content. The chimney complies with the 3-2-10 rule and is located on the inside of the building envelope. What could be the issue? (Page 240)",
+    options: ["The primary air channels could be blocked", "The wood is too dry", "The chimney is too tall", "The damper is fully open"],
+    answer: "The primary air channels could be blocked"
+  },
+  {
+    question: "A customer says there are small bursts of smoke coming through their stove. What are they describing? (Page 240)",
+    options: ["Back Puffing", "Wind-induced Downdraft", "The Catalytic Combustor in action", "A fully open primary air channel"],
+    answer: "Back Puffing"
+  },
+  {
+    question: "You can immediately stop back puffing by DECREASING airflow. (Page 241)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "Barometric draft regulators _________ draft. (Page 242)",
+    options: ["Reduce", "Increase", "Have no effect on"],
+    answer: "Reduce"
+  },
+  {
+    question: "You should always install barometric draft regulators. (Page 242)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    question: "Does the following statement resist or promote spillage? : The chimney flue is not insulated. (Page 243)",
+    options: ["Resists Spillage", "Promotes Spillage"],
+    answer: "Promotes Spillage"
+  },
+  {
+    question: "Does the following statement resist or promote spillage? : The flue is inside the house envelope. (Page 243)",
+    options: ["Resists Spillage", "Promotes Spillage"],
+    answer: "Resists Spillage"
+  },
+  {
+    question: "Does the following statement resist or promote spillage? : The appliance is located below the neutral pressure plane. (Page 243)",
+    options: ["Resists Spillage", "Promotes Spillage"],
+    answer: "Promotes Spillage"
+  },
+  {
+    question: "Does the following statement resist or promote spillage? : The connector has many elbows. (Page 243)",
+    options: ["Resists Spillage", "Promotes Spillage"],
+    answer: "Promotes Spillage"
+  },
+  {
+    question: "Does the following statement resist or promote spillage? : Total system height is less than 15 feet. (Page 243)",
+    options: ["Resists Spillage", "Promotes Spillage"],
+    answer: "Promotes Spillage"
+  },
+  {
+    question: "Does the following statement resist or promote spillage? : The flue size is different than the flue collar size. (Page 243)",
+    options: ["Resists Spillage", "Promotes Spillage"],
+    answer: "Promotes Spillage"
+  },
+  {
+    question: "Does the following statement resist or promote spillage? : The house is clear of wind obstructions. (Page 243)",
+    options: ["Resists Spillage", "Promotes Spillage"],
+    answer: "Resists Spillage"
+  },
+  {
+    question: "Does the following statement resist or promote spillage? : The homeowner is tolerant to wood smoke smell. (Page 243)",
+    options: ["Resists Spillage", "Promotes Spillage"],
+    answer: "Promotes Spillage"
+  },
+  {
+    question: "Does the following statement resist or promote spillage? : The homeowner is an experienced wood burner. (Page 243)",
+    options: ["Resists Spillage", "Promotes Spillage"],
+    answer: "Resists Spillage"
   },
 ];
 
