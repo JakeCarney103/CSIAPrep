9cf3f05168146c0e55ffab25941d12273b6029c6
Jake Carney <jakecarney103@gmail.com>
2026-02-07 11:19:32 -0500

    Add NFPA 211 Chapter 6

---
+++ QuizSources/NFPA_211/chapter6.js
@@ -0,0 +1,42 @@
+const nfpa211Chapter6QuizData = [
+  {
+    id: "6_1",
+    question: "Factory-Built Chimneys and Chimney Units shall be installed in accordance with _________. (NFPA 211 6.1.1)",
+    options: ["The manufacturer's installation instructions", "NFPA 211", "NFPA 54", "IRC"],
+    answer: "The manufacturer's installation instructions"
+  },
+  {
+    id: "6_2",
+    question: "Factory-Built Chimneys for use with wood-burning appliances shall comply with _________. (NFPA 211 6.1.3.2)",
+    options: ["UL 103", "UL 103HT", "UL 127", "UL 1777"],
+    answer: "UL 103HT"
+  },
+  {
+    id: "6_3",
+    question: "Factory-Built Chimneys for use with wood-burning appliances shall comply with _________. (NFPA 211 6.1.3.2)",
+    options: ["UL 103", "UL 103HT", "UL 127", "UL 1777"],
+    answer: "UL 103HT"
+  },
+  {
+    id: "6_4",
+    question: "Factory-Built Chimneys that pass through floors of building requiring the protection of vertical opening shall be enclosed with approved walls having a fire resistance rating of not less than _________ where such chimneys are located in a building less than 4 stories. (NFPA 211 6.1.4)",
+    options: ["30 minutes", "1 hour", "2 hours", "5 hours"],
+    answer: "1 hour"
+  },
+  {
+    id: "6_5",
+    question: "Factory-Built Chimneys that pass through floors of building requiring the protection of vertical opening shall be enclosed with approved walls having a fire resistance rating of not less than _________ where such chimneys are located in a building with 4 or more stories. (NFPA 211 6.1.4)",
+    options: ["30 minutes", "1 hour", "2 hours", "5 hours"],
+    answer: "2 hours"
+  },
+  {
+    id: "6_6",
+    question: "Factory-Built Chimneys and Chimney Units installed through insulation or areas to be insulated shall be separated by a physical barrier to establish and maintain the minimum air space clearance. (NFPA 211 6.4)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+];
+
+if (!window.quizRegistry) window.quizRegistry = {};
+window.quizRegistry['Chapter 6: Factory-Built Chimneys and Chimney Units'] = nfpa211Chapter6QuizData;
+
