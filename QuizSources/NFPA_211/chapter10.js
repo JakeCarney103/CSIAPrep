aa5138a1f6aca5d4f1b770c20bc549c7e09c2593
Jake Carney <jakecarney103@gmail.com>
2026-02-09 20:19:59 -0500

    Add NFPA 211 chapter 10, without Dryer Vent info

---
+++ QuizSources/NFPA_211/chapter10.js
@@ -0,0 +1,60 @@
+const nfpa211Chapter10QuizData = [
+  {
+    id: "10_1",
+    question: "Type B gas vents can be used for vented wall furnaces listed for use with Type BW gas vents only. (NFPA 211 10.1.2)",
+    options: ["True", "False"],
+    answer: "False"
+  },
+  {
+    id: "10_2",
+    question: "What category of gas appliance can Type B vents be used with? (NFPA 211 10.1.1)",
+    options: ["Category I", "Category II", "Category III", "Category IV", "All of these"],
+    answer: "Category I"
+  },
+  {
+    id: "10_3",
+    question: "Single-wall metal pipe shall not originate in any unoccupied attic or concealed space and shall not pass through any attic, inside wall, concealed space, floor, or ceiling. (NFPA 211 10.1.6.4)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    id: "10_4",
+    question: "Vents installed with mechanical exhausters shall not terminate less than _________ above the highest point where they pass through the roof surface. (NFPA 211 10.4.1.2)",
+    options: ["12 inches", "5 feet", "18 inches", "6 inches"],
+    answer: "12 inches"
+  },
+  {
+    id: "10_5",
+    question: "Natural draft vents for gas appliances, other than those in 10.4.3 and 10.7.2, shall terminate at an elevation not less than _________ above the highest connected appliance outlet. (NFPA 211 10.4.2)",
+    options: ["5 feet", "10 feet", "15 feet", "8 feet"],
+    answer: "5 feet"
+  },
+  {
+    id: "10_6",
+    question: "Unlisted gas appliances without draft hoods have a minimum clearance of _________. (NFPA 211 10.6.4.1)",
+    options: ["6 inches", "9 inches", "18 inches", "12 inches"],
+    answer: "18 inches"
+  },
+  {
+    id: "10_7",
+    question: "Unlisted gas appliances with draft hoods have a minimum clearance of _________. (NFPA 211 10.6.4.1)",
+    options: ["6 inches", "9 inches", "18 inches", "12 inches"],
+    answer: "9 inches"
+  },
+  {
+    id: "10_8",
+    question: "Boilers and furnaces equipped with listed conversion gas burners and with draft hoods have a minimum clearance of _________. (NFPA 211 10.6.4.1)",
+    options: ["6 inches", "9 inches", "18 inches", "12 inches"],
+    answer: "9 inches"
+  },
+  {
+    id: "10_9",
+    question: "Listed gas appliances with draft hoods and other Category I gas appliances listed for use with Type B vents have a minimum clearance of _________. (NFPA 211 10.6.4.1)",
+    options: ["6 inches", "9 inches", "18 inches", "12 inches"],
+    answer: "6 inches"
+  },
+];
+
+if (!window.quizRegistry) window.quizRegistry = {};
+window.quizRegistry['Chapter 10: Vents'] = nfpa211Chapter10QuizData;
+
