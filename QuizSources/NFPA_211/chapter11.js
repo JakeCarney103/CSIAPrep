bb997d0207adfedd93d410f0ffa4822e36df3555
Jake Carney <jakecarney103@gmail.com>
2026-02-11 19:08:03 -0500

    finish chapter 11

--- QuizSources/NFPA_211/chapter11.js
+++ QuizSources/NFPA_211/chapter11.js
@@ -71,6 +71,78 @@ const nfpa211Chapter11QuizData = [
     options: ["4 inches", "8 inches", "6 inches", "2 inches"],
     answer: "4 inches"
   },
+  {
+    id: "11_13",
+    question: "Steel fireplace units incorporating a firebox liner of not less than _________ thick steel and an air chamber shall be installed with masonry to provide a total thickness at the back and sides of not less than _________, not less than _________ of which shall be solid masonry. (NFPA 211 11.2.4.1)",
+    options: ["1/8 inch, 4 inches, 2 inches", "1/8 inch, 4 inches, 8 inches", "1/4 inch, 8 inches, 4 inches", "1/4 inch, 6 inches, 3 inches"],
+    answer: "1/4 inch, 8 inches, 4 inches"
+  },
+  {
+    id: "11_14",
+    question: "The back face of a masonry fireplace shall have at least _________ of clearance from combustible materials. (NFPA 211 11.2.5.1)",
+    options: ["4 inches", "8 inches", "6 inches", "2 inches"],
+    answer: "4 inches"
+  },
+  {
+    id: "11_15",
+    question: "If a mantel projects 1.5 inches or less, it shall be at least _________ from the firebox opening. (NFPA 211 11.2.5.3)",
+    options: ["4 inches", "8 inches", "6 inches", "12 inches"],
+    answer: "6 inches"
+  },
+  {
+    id: "11_16",
+    question: "If a mantel projects over 1.5 inches, it shall be at least _________ from the firebox opening. (NFPA 211 11.2.5.4)",
+    options: ["4 inches", "8 inches", "6 inches", "12 inches"],
+    answer: "12 inches"
+  },
+  {
+    id: "11_17",
+    question: "A header above a firebox opening shall be not less than _________ away from the face. (NFPA 211 11.2.5.1)",
+    options: ["4 inches", "8 inches", "2 inches", "12 inches"],
+    answer: "2 inches"
+  },
+  {
+    id: "11_18",
+    question: "Studs to the side of a firebox should be at least _________ away from the firebox. (NFPA 211 11.2.5.1)",
+    options: ["4 inches", "8 inches", "2 inches", "1 inch"],
+    answer: "2 inches"
+  },
+  {
+    id: "11_19",
+    question: "Masonry fireplaces shall have hearth extensions that have at least _________ of clearance from combustible materials directly below the underside. (NFPA 211 11.3.1)",
+    options: ["4 inches", "8 inches", "2 inches", "12 inches"],
+    answer: "4 inches"
+  },
+  {
+    id: "11_20",
+    question: "The minimum thickness of a hearth extension shall be _________. (NFPA 211 11.3.2)",
+    options: ["4 inches", "8 inches", "2 inches", "12 inches"],
+    answer: "2 inches"
+  },
+  {
+    id: "11_21",
+    question: "If the fireplace opening is less than 6 square feet (864 square inches), the hearth extension shall extend at least _________ in front of the facing material and at least _________ beyond each side of the fireplace opening. (NFPA 211 11.3.3)",
+    options: ["16 inches, 8 inches", "8 inches, 16 inches", "20 inches, 12 inches", "16 inches, 16 inches"],
+    answer: "16 inches, 8 inches"
+  },
+  {
+    id: "11_22",
+    question: "If the fireplace opening is 6 square feet (864 square inches) or more, the hearth extension shall extend at least _________ in front of the facing material and at least _________ beyond each side of the fireplace opening. (NFPA 211 11.3.3)",
+    options: ["16 inches, 8 inches", "8 inches, 16 inches", "20 inches, 12 inches", "20 inches, 16 inches"],
+    answer: "20 inches, 12 inches"
+  },
+  {
+    id: "11_23",
+    question: "Combustion air ducts that terminate outside the fire chamber but within 6 inches of the fire chamber shall be designed and installed to prevent the direct entry of flame, embers, or ashes from the fire chamber into the duct. (NFPA 211 11.4.5)",
+    options: ["True", "False"],
+    answer: "True"
+  },
+  {
+    id: "11_24",
+    question: "LISTED combustion air ducts shall be installed with a minimum 1 inch clearance to combustibles for all parts of the duct construction within 5 ft of the duct outlet. (NFPA 211 11.4.6)",
+    options: ["True", "False"],
+    answer: "False"
+  },
 ];
 
 if (!window.quizRegistry) window.quizRegistry = {};
